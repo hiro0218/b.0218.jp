b2cd97c119ae905b8b0a30da87c66dfd18e78d9b
hiro0218 <hiro0218@gmail.com>
2019-01-17 01:29:21 +0900

    fix: footer style

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,17 +1,22 @@
 <template>
   <footer class="o-container">
+    <nav class="menu">
+      <ul class="menu-list">
+        <li class="menu-item">
+          <nuxt-link to="/archive">archive</nuxt-link>
+        </li>
+        <li class="menu-item">
+          <nuxt-link to="/category">category</nuxt-link>
+        </li>
+        <li class="menu-item">
+          <nuxt-link to="/tag">tag</nuxt-link>
+        </li>
+      </ul>
+    </nav>
     <div class="copyright">
-      <small
-      >©
+      <small>©
         <nuxt-link to="/">{{ siteName }}</nuxt-link>
       </small>
-    </div>
-    <div class="menu">
-      <ul>
-        <li><nuxt-link to="/archive">archive</nuxt-link></li>
-        <li><nuxt-link to="/category">category</nuxt-link></li>
-        <li><nuxt-link to="/tag">tag</nuxt-link></li>
-      </ul>
     </div>
   </footer>
 </template>
@@ -25,5 +30,37 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+footer {
+  padding-bottom: 3rem;
+  padding-top: 3rem;
+  background: $oc-gray-8;
+  color: $oc-gray-1;
+  font-size: map-get($size, sm) * 1rem;
+}
+.copyright {
+  small {
+    font-size: 1em;
+  }
+  a {
+    color: inherit;
+  }
+}
+.menu {
+  margin-bottom: 1rem;
+}
+.menu-list {
+  display: inline-flex;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.menu-item {
+  & + & {
+    margin-left: map-get($size, sm) * 1rem;
+  }
+  a {
+    color: $oc-gray-2;
+  }
+}
 </style>
