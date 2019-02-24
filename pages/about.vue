96f1dd4a8820da94c6701bbba1066494ee77cf00
hiro0218 <hiro0218@gmail.com>
2019-02-25 00:46:33 +0900

    add: page/about

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <article class="post">
+    <header class="c-title">
+      <h1 class="title-main">{{ pageTitle }}</h1>
+    </header>
+    <div class="post-content" v-html="about"/>
+  </article>
+</template>
+
+<script>
+import about from '~/assets/page/about.md';
+
+export default {
+  name: 'About',
+  head() {
+    return {
+      title: this.pageTitle,
+    };
+  },
+  computed: {
+    pageTitle: () => 'about',
+    about() {
+      return about;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  margin-bottom: 5rem;
+}
+</style>
