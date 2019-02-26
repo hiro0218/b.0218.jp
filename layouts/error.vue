e87908d7e38149ce75db11dfc4e23ddfc67fa160
hiro0218 <hiro0218@gmail.com>
2019-02-27 01:04:13 +0900

    add: button / fix: message

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,15 +2,26 @@
   <section class="error-container">
     <header class="c-title">
       <h1 class="title-main">
-        <template v-if="error.statusCode >= 400">
-          お探しのページは見つかりませんでした
+        <template v-if="error.statusCode >= 500">
+          予期せぬエラーが発生しました
         </template>
         <template v-else>
-          予期せぬエラーが発生しました
+          お探しのページは見つかりませんでした
         </template>
       </h1>
       <div class="title-sub">{{ error.message }}</div>
     </header>
+    <section class="error-message">
+      <p v-if="error.statusCode >= 500">
+        技術的な問題が発生しているため、このページを表示できません。
+      </p>
+      <p v-else>
+        このページは削除されたかURLが変更されています。
+      </p>
+    </section>
+    <footer>
+      <nuxt-link to="/" class="button">トップページに戻る</nuxt-link>
+    </footer>
   </section>
 </template>
 
@@ -32,6 +43,37 @@ export default {
 
 <style lang="scss">
 .error-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
   text-align: center;
+
+  .c-title {
+    .title-sub {
+      color: $oc-gray-6;
+    }
+  }
+
+  .error-message {
+    margin-bottom: 2rem;
+  }
+
+  .button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    align-self: flex-start;
+    white-space: nowrap;
+    padding: 0.75em 2em;
+    border-radius: 0.15rem;
+    border: 1px solid $oc-gray-3;
+    color: $base-color;
+    font-size: $font-size-xs;
+    &:hover {
+      opacity: 0.6;
+    }
+  }
 }
 </style>
