7ce02909a885e0487342e747e4356a16ca961693
hiro0218 <hiro0218@gmail.com>
2019-02-26 18:27:41 +0900

    fix: title message

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="error-container">
     <header class="c-title">
-      <h1 class="title-main">{{ error.statusCode }}</h1>
+      <h1 class="title-main">
+        <template v-if="error.statusCode >= 400">
+          お探しのページは見つかりませんでした
+        </template>
+        <template v-else>
+          予期せぬエラー 予期せぬエラーが発生しました
+        </template>
+      </h1>
       <div class="title-sub">{{ error.message }}</div>
     </header>
   </section>
