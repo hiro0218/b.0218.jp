83ad399af27e1140e5b32bd9f5ffeed8719c5e20
hiro0218 <hiro0218@gmail.com>
2019-03-27 18:50:12 +0900

    init: 記事のレイアウト用コンポーネントを追加

---
+++ components/LayoutArticle.vue
@@ -0,0 +1,20 @@
+<template>
+  <article class="post">
+    <header class="c-title">
+      <h1 class="title-main">
+        <slot name="postTitle"/>
+      </h1>
+      <slot name="postMeta"/>
+    </header>
+    <slot/>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'LayoutArticle',
+};
+</script>
+
+<style>
+</style>
