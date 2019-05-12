7469b1ca5476d70b804337e210dfe1756fa1b04f
hiro0218 <hiro0218@gmail.com>
2019-05-12 09:34:32 +0900

    add: static data

---
+++ ouputStaticData.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const consola = require('consola');
+
+// constant
+const ENDPOINT = 'https://content.b.0218.jp/wp-json/';
+const DIST_DIRECTRY = './dist/api/';
+
+// axios setting
+const axios = require('axios');
+const client = axios.create({
+  baseURL: ENDPOINT,
+});
+
+// functions
+const prepareDistDirectory = async () => {
+  await mkdirp(DIST_DIRECTRY);
+};
+
+const writeJsonFile = (filename, data) => {
+  fs.writeFile(DIST_DIRECTRY + filename, JSON.stringify(data), err => {
+    if (err) {
+      consola.error(err);
+    } else {
+      consola.success(filename);
+    }
+  });
+};
+
+async function outputArchive() {
+  const res = await client.get('0218/v1/archive');
+  writeJsonFile('archive.json', res.data);
+}
+
+async function outputCategoryList() {
+  const res = await client.get('wp/v2/categories', {
+    params: {
+      order: 'desc',
+      orderby: 'count',
+    },
+  });
+  writeJsonFile('categories.json', res.data);
+}
+
+// do
+prepareDistDirectory();
+outputArchive();
+outputCategoryList();
