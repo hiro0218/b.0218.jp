3d2b1205f9e83edcbc7e919174280e15ffc62cbd
hiro0218 <hiro0218@gmail.com>
2019-05-14 16:11:42 +0900

    add: LayoutPostsList

---
+++ components/LayoutPostsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div class="c-title is-center">
+      <h1 class="title-main">
+        <slot name="postsListTitle" />
+      </h1>
+      <div class="title-sub">
+        <slot name="postsListTitleSub" />
+      </div>
+    </div>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LayoutPostsList',
+};
+</script>
+
+<style lang="scss" scoped></style>
