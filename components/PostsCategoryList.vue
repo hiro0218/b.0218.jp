642330bb5f0f7bca885fbccdf72335d6c5dda070
hiro0218 <hiro0218@gmail.com>
2019-07-01 00:16:14 +0900

    fix: category list style

--- components/PostsCategoryList.vue
+++ components/PostsCategoryList.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="categoryList.length !== 0" class="u-scroll-x category-list">
     <div class="category-item">
-      <nuxt-link to="/" exact :class="{ 'nuxt-link-active': this.$route.path == '/' }">すべて</nuxt-link>
+      <nuxt-link to="/" exact :class="{ 'nuxt-link-active': this.$route.path == '/' }">All</nuxt-link>
     </div>
     <template v-for="(item, index) in categoryList">
-      <div v-if="index <= 10" :key="item.id" class="category-item">
+      <div v-if="index <= 5" :key="item.id" class="category-item">
         <nuxt-link :to="'/categories/' + item.slug">{{ item.name }}</nuxt-link>
       </div>
     </template>
@@ -29,30 +29,32 @@ export default {
   margin: 3rem auto;
   display: flex;
   align-items: center;
-  white-space: nowrap;
   font-size: $font-size-sm;
+  letter-spacing: 0.1em;
+  white-space: nowrap;
 }
 
 .category-item {
   text-align: center;
 
   & + .category-item {
-    margin-left: 0.5rem;
+    margin-left: 1rem;
   }
 
   a {
     display: block;
-    width: 10em;
-    padding: 0 0.5rem 1rem;
-    border-bottom: 1px solid $base-color;
-    font-weight: bold;
+    padding: 0.5rem 0;
+    border-bottom: 2px solid transparent;
     opacity: 0.4;
+    transition: opacity 0.3s, border 0.8s;
 
     &:hover {
+      border-bottom-color: $base-color;
       opacity: 1;
     }
 
     &.nuxt-link-active {
+      border-bottom-color: $base-color;
       opacity: 1;
     }
   }
