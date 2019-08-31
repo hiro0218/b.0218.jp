0a51e6f4303df00d5b436f40e61174d93793e74d
hiro0218 <hiro0218@gmail.com>
2019-09-01 01:16:49 +0900

    fix: list style

--- components/post/PostMeta.vue
+++ components/post/PostMeta.vue
@@ -1,33 +1,35 @@
 <template>
   <aside class="post-meta">
-    <ul class="c-meta-list">
-      <li class="meta-item">
-        <font-awesome-icon icon="clock" />
+    <div class="c-meta-list">
+      <font-awesome-icon icon="clock" />
+      <div class="meta-item">
         <time :datetime="post.date" itemprop="datePublished">{{ post.date | formatDateString }}</time>
-      </li>
-      <li v-if="!isDateSameDay(post.date, post.modified)" class="meta-item">
+      </div>
+      <template v-if="!isDateSameDay(post.date, post.modified)">
         <font-awesome-icon icon="angle-right" />
-        <time :datetime="post.modified" itemprop="dateModified">{{ post.modified | formatDateString }}</time>
-      </li>
-    </ul>
-    <ul v-if="post.hasOwnProperty('_embedded')" class="c-meta-list">
-      <li v-for="(category, index) in post._embedded['wp:term'][0]" :key="index" class="meta-item">
-        <font-awesome-icon v-if="index === 0" icon="folder" />
-        <nuxt-link :to="'/categories/' + category.slug">
-          {{ category.name }}
-        </nuxt-link>
-        <span v-if="index !== post._embedded['wp:term'][0].length - 1">,&nbsp;</span>
-      </li>
-    </ul>
-    <ul v-if="post.hasOwnProperty('_embedded')" class="c-meta-list">
-      <li v-for="(post_tag, index) in post._embedded['wp:term'][1]" :key="index" class="meta-item">
-        <font-awesome-icon v-if="index === 0" icon="tag" />
-        <nuxt-link :to="'/tags/' + post_tag.slug">
-          {{ post_tag.name }}
-        </nuxt-link>
-        <span v-if="index !== post._embedded['wp:term'][1].length - 1">,&nbsp;</span>
-      </li>
-    </ul>
+        <div class="meta-item">
+          <time :datetime="post.modified" itemprop="dateModified">{{ post.modified | formatDateString }}</time>
+        </div>
+      </template>
+    </div>
+    <div v-if="post.hasOwnProperty('_embedded') && post._embedded['wp:term'][0].length" class="c-meta-list">
+      <font-awesome-icon icon="folder" />
+      <template v-for="(category, index) in post._embedded['wp:term'][0]">
+        <div :key="index" class="meta-item">
+          <nuxt-link :to="'/categories/' + category.slug">{{ category.name }}</nuxt-link
+          ><span v-if="index !== post._embedded['wp:term'][0].length - 1">,&nbsp;</span>
+        </div>
+      </template>
+    </div>
+    <div v-if="post.hasOwnProperty('_embedded') && post._embedded['wp:term'][1].length" class="c-meta-list">
+      <font-awesome-icon icon="tag" />
+      <template v-for="(post_tag, index) in post._embedded['wp:term'][1]">
+        <div :key="index" class="meta-item">
+          <nuxt-link :to="'/tags/' + post_tag.slug">{{ post_tag.name }}</nuxt-link
+          ><span v-if="index !== post._embedded['wp:term'][1].length - 1">,&nbsp;</span>
+        </div>
+      </template>
+    </div>
   </aside>
 </template>
 
