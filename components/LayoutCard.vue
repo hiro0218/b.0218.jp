8068d51a4101a74243c9a99fb4057022c9cdeb30
hiro0218 <hiro0218@gmail.com>
2019-11-17 23:23:42 +0900

    update: BEMding

--- components/LayoutCard.vue
+++ components/LayoutCard.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="card">
-    <div class="card-image">
-      <img v-if="thumbnail" :src="thumbnail" :alt="title" loading="lazy" />
-      <font-awesome-icon v-else class="no-image" icon="image" />
+  <div class="c-card">
+    <div class="c-card-image">
+      <img v-if="thumbnail" class="c-card-image__item" :src="thumbnail" :alt="title" loading="lazy" />
+      <font-awesome-icon v-else class="c-card-image__no-item" icon="image" />
     </div>
-    <div class="card-body">
-      <div class="card-title">
+    <div class="c-card-body">
+      <div class="c-card-body__title">
         {{ title }}
       </div>
-      <div class="card-description">
+      <div class="c-card-body__description">
         {{ description }}
       </div>
     </div>
-    <footer class="card-footer">
+    <footer class="c-card-footer">
       <slot name="card-footer" />
     </footer>
   </div>
@@ -40,76 +40,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.card {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.card-image {
-  display: flex;
-  flex-basis: 5rem;
-  align-items: center;
-  justify-content: center;
-  width: 5rem;
-  height: 5rem;
-  margin-right: 1rem;
-  overflow: hidden;
-  border-radius: 0.15rem;
-  background-color: map-get($light-color, 3);
-
-  img {
-    width: 5rem;
-    height: 5rem;
-    object-fit: contain;
-  }
-  .no-image {
-    width: 2rem;
-    height: 2rem;
-    color: $tertiary-color;
-  }
-
-  @include mobile {
-    flex-basis: 8rem;
-    width: 8rem;
-    height: 8rem;
-    .no-image {
-      width: 3rem;
-      height: 3rem;
-    }
-  }
-}
-
-.card-body {
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  justify-content: space-between;
-  font-size: $font-size-sm;
-
-  .card-title {
-    margin-bottom: 0.5rem;
-    font-size: 1rem;
-    font-weight: bold;
-    line-height: 1.8;
-  }
-
-  .card-description {
-    flex: 1;
-    margin-bottom: 1rem;
-    color: $secondary-color;
-    letter-spacing: 0.025em;
-    line-height: 1.8;
-    word-break: break-all;
-  }
-}
-
-.card-footer {
-  flex-basis: 100%;
-  color: $tertiary-color;
-  font-size: $font-size-sm;
-  letter-spacing: 0.025em;
-  text-align: right;
-}
-</style>
