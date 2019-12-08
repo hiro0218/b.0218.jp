a52248c3fbadd81e379879227bb186ac761eff35
hiro0218 <hiro0218@gmail.com>
2019-12-09 02:32:50 +0900

    fix: description定義を追加

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -4,6 +4,9 @@
       <h1 class="c-heading__title">
         <slot name="header-title" />
       </h1>
+      <div class="c-heading__description">
+        <slot name="header-description" />
+      </div>
     </div>
     <slot />
   </header>
