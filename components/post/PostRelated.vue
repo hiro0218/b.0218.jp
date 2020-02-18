543e6bf227dde15dee05d7ec173c02c516873c84
hiro0218 <hiro0218@gmail.com>
2020-02-18 18:27:35 +0900

    fix: 関連記事のスタイル変更

--- components/post/PostRelated.vue
+++ components/post/PostRelated.vue
@@ -3,28 +3,19 @@
     <header class="post-related-header">
       <h2 class="post-related-header__title">関連記事</h2>
     </header>
-    <div class="o-post-list">
-      <template v-for="post in related">
-        <router-link :key="post.id" :to="post.url" class="o-post-list__item">
-          <LayoutCard :title="post.title" :thumbnail="post.image">
-            <template v-slot:card-footer>
-              <time :datetime="post.date">{{ post.date | formatDateString }}</time>
-            </template>
-          </LayoutCard>
+    <ul>
+      <li v-for="post in related" :key="post.id">
+        <router-link :to="post.url">
+          {{ post.title }}
         </router-link>
-      </template>
-    </div>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
-import LayoutCard from '~/components/LayoutCard.vue';
-
 export default {
   name: 'PostRelated',
-  components: {
-    LayoutCard,
-  },
   props: {
     related: {
       type: Array,
@@ -34,10 +25,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.post-related-header {
-  margin-bottom: 3rem;
-  text-align: center;
-}
-</style>
