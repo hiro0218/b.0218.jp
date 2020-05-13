6fd2c555192d9f2f761db5265422f9d16e8e3e76
hiro0218 <hiro0218@gmail.com>
2020-05-14 00:54:44 +0900

    fix: mokuji style

--- components/post/PostData.vue
+++ components/post/PostData.vue
@@ -39,28 +39,30 @@ export default {
       }
 
       const container = document.createElement('div');
-      container.classList.add('mokuji-container');
+      container.classList.add('c-mokuji');
 
       // details/summary要素を作成
       const details = document.createElement('details');
       const summary = document.createElement('summary');
-      summary.textContent = 'INDEX';
       details.appendChild(summary);
-      details.classList.add('mokuji-content');
+      details.setAttribute('open', 'true');
 
       // 目次一覧を作成
-      const mokujiData = new this.$mokuji(elPostContent, {
+      const mokujiList = new this.$mokuji(elPostContent, {
         anchorType: true,
         anchorLink: true,
         anchorLinkSymbol: '#',
         anchorLinkBefore: false,
         anchorLinkClassName: 'anchor',
       });
+      mokujiList.classList.add('c-mokuji__list');
 
-      // 要素を追加
-      details.appendChild(mokujiData);
-      container.appendChild(details);
-      separate.insertBefore(container, separate.firstChild);
+      window.requestAnimationFrame(() => {
+        // 要素を追加
+        details.appendChild(mokujiList);
+        container.appendChild(details);
+        separate.insertBefore(container, separate.firstChild);
+      });
     },
   },
 };
@@ -69,46 +71,6 @@ export default {
 <style lang="scss">
 .post__content {
   margin: 2rem 0;
-
-  // mokuji
-  .mokuji-container {
-    margin: 2rem 0;
-    border-radius: 0.15rem;
-    background: $bg-color--light;
-    color: $color-text--light;
-    font-size: $font-size-sm;
-  }
-
-  .mokuji-content {
-    a {
-      color: inherit;
-    }
-
-    ol {
-      margin-bottom: 0;
-      padding-left: 0;
-      list-style-position: inside;
-      counter-reset: number;
-
-      li {
-        list-style: none;
-        &::before {
-          content: counters(number, '-') '. ';
-          counter-increment: number;
-        }
-      }
-
-      ol {
-        margin: 0.5rem 0;
-        padding-left: 1.25em;
-        list-style: none;
-      }
-    }
-
-    > ol > li > a {
-      font-weight: bold;
-    }
-  }
 
   // sandbox
   .sandbox {
