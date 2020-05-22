fa7d2f9ceffb5d71a204cb62d88fdef71c6686f4
hiro0218 <hiro0218@gmail.com>
2020-05-22 15:17:40 +0900

    add: project layer -> header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="pj-header js-header">
-    <div class="o-container pj-header__container">
+    <div class="o-container pj-header-container">
       <nuxt-link :title="siteName" to="/" class="pj-header__logo" v-html="svgLogo" />
       <a
         href="https://www.google.com/search?q=site:b.0218.jp"
         target="_blank"
         rel="noopener"
         title="site:b.0218.jp - Google 検索"
-        class="c-search"
+        class="pj-header-search"
       >
-        <font-awesome-icon icon="search" class="c-search__icon" />
+        <font-awesome-icon icon="search" class="pj-header-search__icon" />
       </a>
     </div>
   </header>
@@ -68,62 +68,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss">
-.pj-header {
-  position: fixed;
-  z-index: $zIndex-header;
-  top: 0;
-  right: 0;
-  left: 0;
-  height: $header-height;
-  transition: transform 0.25s ease;
-  border-bottom: 1px solid $border-color;
-  background: #fff;
-  will-change: transform;
-
-  &.is-unpin {
-    transform: translateY(-$header-height);
-    box-shadow: none;
-  }
-}
-
-.pj-header__container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 100%;
-}
-
-.pj-header__logo {
-  display: flex;
-  align-items: center;
-  height: 100%;
-  color: $color-text;
-
-  &:hover {
-    opacity: 0.6;
-  }
-
-  svg {
-    width: 5rem;
-    height: 100%;
-    transition: width 0.2s;
-    fill: $color-text;
-  }
-}
-
-.c-search {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 3rem;
-  height: 100%;
-}
-
-.c-search__icon {
-  width: 1rem;
-  height: 1rem;
-  color: $color-text--light;
-}
-</style>
