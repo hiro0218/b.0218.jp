cd1d2ebc0c957d93a12843f3827fdc7e4332a6ae
hiro0218 <hiro0218@gmail.com>
2020-05-22 15:21:27 +0900

    fix: vue -> scss

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,22 +4,22 @@
       <button aria-label="scrollButton" class="pj-footer__scroll-button" @click="scrollTop">
         <font-awesome-icon icon="arrow-up" />
       </button>
-      <nav class="pj-footer__menu">
-        <ul class="u-list-unstyled pj-footer__menu-list">
-          <li class="pj-footer__menu-item">
+      <nav class="pj-footer-menu">
+        <ul class="u-list-unstyled pj-footer-menu__list">
+          <li class="pj-footer-menu__item">
             <nuxt-link to="/about" class="pj-footer__link">
               about
             </nuxt-link>
           </li>
-          <li class="pj-footer__menu-item">
+          <li class="pj-footer-menu__item">
             <nuxt-link to="/archive" class="pj-footer__link">
               archive
             </nuxt-link>
           </li>
         </ul>
       </nav>
-      <div class="pj-footer__copyright">
-        <small class="pj-footer__copyright__text">
+      <div class="pj-footer-copyright">
+        <small class="pj-footer-copyright__text">
           ©
           <nuxt-link to="/" class="pj-footer__link">{{ siteName }}</nuxt-link>
         </small>
@@ -50,61 +50,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss">
-.pj-footer {
-  padding: 3rem 0;
-  background: $color-text;
-  color: $bg-color;
-  font-size: $font-size-sm;
-  text-align: center;
-}
-
-.pj-footer__link {
-  color: inherit;
-
-  &:hover {
-    text-decoration: underline;
-  }
-}
-
-// scroll top button
-.pj-footer__scroll-button {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 1.5rem;
-  border: none;
-  background: none;
-  color: inherit;
-  cursor: pointer;
-  appearance: none;
-
-  svg {
-    width: 1.5rem;
-    height: 1.5rem;
-  }
-}
-
-// menu
-.pj-footer__menu {
-  margin-bottom: 1rem;
-}
-
-.pj-footer__menu-list {
-  display: inline-flex;
-}
-
-.pj-footer__menu-item {
-  & + & {
-    margin-left: map-get($size, sm) * 1rem;
-  }
-}
-
-// copyright
-.pj-footer__copyright {
-  &__text {
-    font-size: 1em;
-  }
-}
-</style>
