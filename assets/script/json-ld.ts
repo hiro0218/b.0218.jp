1d7a88bd65d6aa387f5d25a9ccff86368fa28283
hiro0218 <hiro0218@gmail.com>
2020-05-23 16:52:44 +0900

    change: 処理をjson-ldファイルへ分割

---
+++ assets/script/json-ld.ts
@@ -0,0 +1,104 @@
+import CONSTANT from '../../constant';
+
+interface Post {
+  date: string;
+  updated: string;
+  slug: string;
+  link: string;
+  title: string;
+  content: string;
+  excerpt: string;
+  thumbnail: string;
+  categories: [];
+  tags: [];
+  next: {};
+  prev: {};
+}
+
+const getDescriptionText = (postContent: string): string => {
+  let content = postContent;
+
+  // strip line break
+  content = content.replace(/(?:\r\n|\r|\n)/g, '');
+
+  // strip tag
+  content = content.replace(/<\/?[^>]+(>|$)/g, ' ');
+
+  // character extraction
+  content = content.substring(0, 140);
+
+  return content;
+};
+
+export const getBlogPostingStructured = (post: Post): string => {
+  return JSON.stringify({
+    '@context': 'http://schema.org',
+    '@type': 'BlogPosting',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': `${CONSTANT.SITE_URL}${post.slug}`,
+    },
+    headline: post.title,
+    datePublished: post.date,
+    dateModified: post.updated,
+    author: { '@type': 'Person', name: CONSTANT.AUTHOR },
+    description: getDescriptionText(post.content),
+    image: {
+      '@type': 'ImageObject',
+      url: post.thumbnail,
+      width: 696,
+      height: 696,
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: CONSTANT.SITE_NAME,
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://b.0218.jp/hiro0218.png',
+        width: '400px',
+        height: '400px',
+      },
+    },
+  });
+};
+
+export const getBreadcrumbStructured = (post: Post): string => {
+  let itemCount = 1;
+  const itemListElement = [
+    {
+      '@type': 'ListItem',
+      position: itemCount,
+      item: { '@id': CONSTANT.SITE_URL, name: CONSTANT.SITE_NAME },
+    },
+  ];
+
+  if (post.categories) {
+    for (let i = 0; i < post.categories.length; i++) {
+      const category: { [key: string]: string } = post.categories[i];
+      itemListElement.push({
+        '@type': 'ListItem',
+        position: ++itemCount,
+        item: {
+          '@id': `${CONSTANT.SITE_URL}/${category.path}`,
+          name: category.name,
+        },
+      });
+    }
+  }
+
+  itemListElement.push({
+    '@type': 'ListItem',
+    position: ++itemCount,
+    item: { '@id': post.link, name: post.title },
+  });
+
+  const structure = Object.assign(
+    {
+      '@context': 'http://schema.org',
+      '@type': 'BreadcrumbList',
+    },
+    { itemListElement: itemListElement },
+  );
+
+  return JSON.stringify(structure);
+};
--- pages/_post.vue
+++ pages/_post.vue
@@ -22,6 +22,9 @@
 </template>
 
 <script>
+import CONSTANT from '~/constant';
+import { getBlogPostingStructured, getBreadcrumbStructured } from '~/assets/script/json-ld';
+
 export default {
   name: 'Post',
   validate({ params }) {
@@ -66,95 +69,6 @@ export default {
 
     error({ statusCode: 404, message: 'Page not found' });
   },
-  computed: {
-    descriptionText: function () {
-      let content = this.post.content;
-
-      // strip line break
-      content = content.replace(/(?:\r\n|\r|\n)/g, '');
-
-      // strip tag
-      content = content.replace(/<\/?[^>]+(>|$)/g, ' ');
-
-      // character extraction
-      content = content.substring(0, 140);
-
-      return content;
-    },
-  },
-  methods: {
-    getBlogPostingStructured() {
-      return JSON.stringify({
-        '@context': 'http://schema.org',
-        '@type': 'BlogPosting',
-        mainEntityOfPage: {
-          '@type': 'WebPage',
-          '@id': `${process.env.SITE_URL}${this.post.slug}`,
-        },
-        headline: this.post.title,
-        datePublished: this.post.date,
-        dateModified: this.post.updated,
-        author: { '@type': 'Person', name: process.env.AUTHOR },
-        description: this.descriptionText,
-        image: {
-          '@type': 'ImageObject',
-          url: this.post.thumbnail,
-          width: 696,
-          height: 696,
-        },
-        publisher: {
-          '@type': 'Organization',
-          name: process.env.SITE_NAME,
-          logo: {
-            '@type': 'ImageObject',
-            url: 'https://b.0218.jp/hiro0218.png',
-            width: '400px',
-            height: '400px',
-          },
-        },
-      });
-    },
-    getBreadcrumbStructured() {
-      let itemCount = 1;
-      const itemListElement = [
-        {
-          '@type': 'ListItem',
-          position: itemCount,
-          item: { '@id': process.env.SITE_URL, name: process.env.SITE_NAME },
-        },
-      ];
-
-      if (this.post.categories) {
-        for (let i = 0; i < this.post.categories.length; i++) {
-          const category = this.post.categories[i];
-          itemListElement.push({
-            '@type': 'ListItem',
-            position: ++itemCount,
-            item: {
-              '@id': `${process.env.SITE_URL}/${category.path}`,
-              name: category.name,
-            },
-          });
-        }
-      }
-
-      itemListElement.push({
-        '@type': 'ListItem',
-        position: ++itemCount,
-        item: { '@id': this.post.link, name: this.post.title },
-      });
-
-      const structure = Object.assign(
-        {
-          '@context': 'http://schema.org',
-          '@type': 'BreadcrumbList',
-        },
-        { itemListElement: itemListElement },
-      );
-
-      return JSON.stringify(structure);
-    },
-  },
   head() {
     return {
       __dangerouslyDisableSanitizers: ['script'],
@@ -162,23 +76,23 @@ export default {
       meta: [
         { hid: 'description', name: 'description', content: this.descriptionText },
         { hid: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:url', property: 'og:url', content: `${process.env.SITE_URL}${this.post.slug}` },
+        { hid: 'og:url', property: 'og:url', content: `${CONSTANT.SITE_URL}${this.post.slug}` },
         { hid: 'og:title', property: 'og:title', content: this.post.title },
         { hid: 'og:description', property: 'og:description', content: this.descriptionText },
-        { hid: 'og:image', property: 'og:image', content: this.post.thumbnail || process.env.AUTHOR_ICON },
+        { hid: 'og:image', property: 'og:image', content: this.post.thumbnail || CONSTANT.AUTHOR_ICON },
         { hid: 'og:updated_time', property: 'og:updated_time', content: this.post.updated },
         { hid: 'article:published_time', property: 'article:published_time', content: this.post.date },
         { hid: 'article:modified_time', property: 'article:modified_time', content: this.post.updated },
       ],
-      link: [{ rel: 'canonical', href: `${process.env.SITE_URL}${this.post.slug}` }],
+      link: [{ rel: 'canonical', href: `${CONSTANT.SITE_URL}${this.post.slug}` }],
       script: [
         {
           type: 'application/ld+json',
-          innerHTML: this.getBlogPostingStructured(),
+          innerHTML: getBlogPostingStructured(this.post),
         },
         {
           type: 'application/ld+json',
-          innerHTML: this.getBreadcrumbStructured(),
+          innerHTML: getBreadcrumbStructured(this.post),
         },
       ],
     };
