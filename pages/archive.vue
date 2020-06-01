f48348341fc677f930f0d9950889ca98d8b7e2e3
hiro0218 <hiro0218@gmail.com>
2020-06-01 19:12:56 +0900

    update: アーカイブ年を表示するよう変更

--- pages/archive.vue
+++ pages/archive.vue
@@ -9,12 +9,19 @@
       </template>
     </LayoutHeader>
     <section>
-      <ul class="archive-list">
-        <li v-for="(post, key) in $source.archives" :key="key" class="archive-item">
-          <time :datetime="post.date" class="archive-item__date">{{ convertDateToSimpleFormat(post.date) }}</time>
-          <a :href="post.path" class="archive-item__link">{{ post.title }}</a>
-        </li>
-      </ul>
+      <div v-for="(archive, yaer) in archives" :key="yaer" class="archive-list">
+        <div class="archive-year">
+          <h2 class="archive-year__title">{{ yaer }}</h2>
+        </div>
+        <div class="archive-post">
+          <div v-for="(post, index) in archive" :key="index" class="archive-post__list">
+            <a :href="post.path" class="archive-post__link">
+              <div class="archive-post__title">{{ post.title }}</div>
+              <time :datetime="post.date" class="archive-post__date">{{ convertDateToSimpleFormat(post.date) }}</time>
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   </article>
 </template>
@@ -23,16 +30,39 @@
 import { defineComponent, computed } from '@vue/composition-api';
 import { convertDateToSimpleFormat } from '~/assets/script/utils/date.ts';
 
+const formatArchive = (srcArchives) => {
+  const archives = {};
+
+  for (let i = 0; i < srcArchives.length; i++) {
+    const post = srcArchives[i];
+
+    // 日付を取得する
+    const date = new Date(post.date);
+    const year = date.getFullYear();
+
+    // 配列で初期化
+    if (!Array.isArray(archives[year])) {
+      archives[year] = [];
+    }
+
+    archives[year].push(post);
+  }
+
+  return archives;
+};
+
 export default defineComponent({
   name: 'Archive',
-  setup() {
+  setup(_, { root }) {
     const pageTitle = computed(() => 'Archive');
     const pageDescription = computed(() => 'これまでに公開した記事の一覧です。');
+    const archives = computed(() => formatArchive(root.$source.archives));
 
     return {
       pageTitle,
       pageDescription,
       convertDateToSimpleFormat,
+      archives,
     };
   },
   head() {
