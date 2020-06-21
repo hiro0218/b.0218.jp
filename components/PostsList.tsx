c9887d77f8749d843b7dafccbbb2db82abf5d4e1
hiro0218 <hiro0218@gmail.com>
2020-06-21 16:55:51 +0900

    change: vue -> tsx

---
+++ components/PostsList.tsx
@@ -0,0 +1,29 @@
+export default {
+  name: 'PostsList',
+  props: {
+    posts: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+  render() {
+    return (
+      <div class="post-list">
+        {this.posts.map((post) => (
+          <router-link to={'/' + post.path} class="c-card">
+            <div class="c-card-image">
+              <div class="c-card-image__container">
+                <div class="c-card-image__no-item" domPropsInnerHTML={this.$icon.image.toSVG()} />
+              </div>
+            </div>
+            <div class="c-card-body">
+              <div class="c-card-body__title">{post.title}</div>
+              {post.excerpt && <div class="c-card-body__description">{post.excerpt}</div>}
+            </div>
+          </router-link>
+        ))}
+      </div>
+    );
+  },
+};
