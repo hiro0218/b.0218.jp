a82757e9f9af46c18da05fc94fabe0f2ff83cd99
hiro0218 <hiro0218@gmail.com>
2020-09-13 19:11:46 +0900

    refact: ハイライト対象を調整

--- plugins/filteredPost.ts
+++ plugins/filteredPost.ts
@@ -9,16 +9,20 @@ export default (_, inject) => {
 
     // highlight.js
     $('pre code').each((_, element) => {
-      const elementClass = $(element).attr('class');
-      const className = elementClass ? elementClass.replace('language-', '') : '';
-      const result = hljs.highlightAuto($(element).text(), [className]);
+      const $element = $(element);
+      const elementClass = $element.attr('class');
 
-      if (className) {
-        $(element).attr('data-language', className);
+      if (elementClass) {
+        const className = elementClass ? elementClass.replace('language-', '') : '';
+        const result = hljs.highlightAuto($element.text(), [className]);
+
+        if (className) {
+          $element.attr('data-language', className);
+        }
+
+        $element.addClass('hljs');
+        $element.html(result.value);
       }
-
-      $(element).addClass('hljs');
-      $(element).html(result.value);
     });
 
     // wrap table
