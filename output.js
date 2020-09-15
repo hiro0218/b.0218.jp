41fc91dd7ed28a59ea106047d8ff98da6fd6ac1a
hiro0218 <hiro0218@gmail.com>
2020-09-15 20:37:54 +0900

    add: comment

--- output.js
+++ output.js
@@ -5,6 +5,7 @@ const SRC_DIR = './_source';
 
 const copyFile = (filename) => {
   fs.copyFileSync(`${SRC_DIR}/${filename}`, `${DEST_DIR}/${filename}`);
+  console.log(`output.js: ${SRC_DIR}/${filename} to ${DEST_DIR}/${filename}`);
 };
 
 try {
