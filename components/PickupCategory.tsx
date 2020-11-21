e0b645b454dfc14ca710093aaefd7a6e325ecfca
hiro0218 <hiro0218@gmail.com>
2020-11-21 15:06:32 +0900

    fix: style

--- components/PickupCategory.tsx
+++ components/PickupCategory.tsx
@@ -14,16 +14,6 @@ export default defineComponent({
       <div class="u-scroll-x c-pickup-category">
         <router-link to="/" exact class="c-pickup-category__link">
           <div class="c-pickup-category__item">
-            <div class="c-pickup-category__logo">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-            </div>
             <div class="c-pickup-category__name">HOME</div>
           </div>
         </router-link>
@@ -32,9 +22,6 @@ export default defineComponent({
           .map((item) => (
             <router-link to={'/' + item.path} class="c-pickup-category__link">
               <div key={item.id} class="c-pickup-category__item">
-                <div class="c-pickup-category__logo">
-                  <img src={item.logo} alt={item.name} loading="lazy" />
-                </div>
                 <div class="c-pickup-category__name">{item.name}</div>
               </div>
             </router-link>
