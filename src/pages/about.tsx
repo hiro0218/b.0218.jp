5072e02a845009212839b2f2b4bb31af94916064
hiro0218 <hiro0218@gmail.com>
2020-12-06 21:35:53 +0900

    change: vue -> tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,39 @@
+import { defineComponent, useMeta, useAsync } from '@nuxtjs/composition-api';
+
+import LayoutHeader from '../components/LayoutHeader';
+
+const pageTitle = 'About';
+const pageDescription = 'サイトと運営者の情報';
+
+export default defineComponent({
+  name: 'About',
+  setup(_, { root }) {
+    useMeta({
+      title: pageTitle,
+      meta: [{ hid: 'description', name: 'description', content: pageDescription }],
+    });
+
+    const content = useAsync(() => {
+      // @ts-ignore
+      const page = root.context.$source.pages.find((page: any) => page.slug === 'about');
+
+      // @ts-ignore
+      return root.context.$filteredPost(page.content);
+    });
+
+    return {
+      content,
+    };
+  },
+  head() {
+    return {};
+  },
+  render() {
+    return (
+      <article class="post">
+        <LayoutHeader title={pageTitle} description={pageDescription} />
+        <div class="post__content js-post-content" domPropsInnerHTML={this.content} />
+      </article>
+    );
+  },
+});
