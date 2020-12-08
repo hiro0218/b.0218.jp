849c3c9e30e194fa395e12b58c16b9f4a9ba0a2c
hiro0218 <hiro0218@gmail.com>
2020-12-08 21:33:54 +0900

    add: ts/parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
   extends: ['@nuxtjs/eslint-config-typescript', '@hiro0218/eslint-config'],
 };
