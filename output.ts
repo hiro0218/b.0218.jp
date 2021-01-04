64d909e246ac3775297ce9e01ced40a3e224017b
hiro0218 <hiro0218@gmail.com>
2021-01-04 11:51:18 +0900

    change: js -> ts

--- output.ts
+++ output.ts
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 
 const DEST_DIR = './src/static';
 const SRC_DIR = './src/_source';
 
-const copyFile = (filename) => {
+const copyFile = (filename: string) => {
   fs.copyFileSync(`${SRC_DIR}/${filename}`, `${DEST_DIR}/${filename}`);
   console.log(`output.js: ${SRC_DIR}/${filename} to ${DEST_DIR}/${filename}`);
 };
