64b56d69fe3ce63b5758b391a93a81a4d3f3d2da
hiro0218 <hiro0218@gmail.com>
2021-03-22 14:32:54 +0900

    add: ヘッダーの固定処理を追加

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -1,10 +1,56 @@
 import Link from 'next/link';
+import React, { useEffect } from 'react';
 
 import Logo from '@/images/logo.svg';
 
+const initUnpinHeader = () => {
+  const elHeader = document.querySelector<HTMLDivElement>('.js-header');
+  const headerHeight = elHeader.offsetHeight;
+  const headerUnpinClassName = 'is-unpin';
+  let ticking = false;
+  let lastKnownScrollY = 0;
+
+  const handleScroll = (elHeader: HTMLDivElement, headerHeight: number) => {
+    if (!ticking) {
+      requestAnimationFrame(() => {
+        ticking = false;
+        const currentScrollY = window.pageYOffset;
+
+        // ヘッダーの高さを超えた場合
+        if (currentScrollY >= headerHeight) {
+          if (currentScrollY <= lastKnownScrollY) {
+            elHeader.classList.remove(headerUnpinClassName);
+          } else {
+            elHeader.classList.add(headerUnpinClassName);
+          }
+        } else {
+          elHeader.classList.remove(headerUnpinClassName);
+        }
+
+        // 今回のスクロール位置を残す
+        lastKnownScrollY = currentScrollY;
+      });
+    }
+
+    ticking = true;
+  };
+
+  document.addEventListener(
+    'scroll',
+    () => {
+      handleScroll(elHeader, headerHeight);
+    },
+    { passive: false },
+  );
+};
+
 const TheHeader = () => {
+  useEffect(() => {
+    initUnpinHeader();
+  });
+
   return (
-    <header className="pj-header">
+    <header className="pj-header js-header">
       <div className="l-container pj-header-container">
         <Link href="/">
           <a className="pj-header__logo">
