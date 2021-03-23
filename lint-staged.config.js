120b633a052d831a6d28f263be078cae29e5ea0a
hiro0218 <hiro0218@gmail.com>
2021-03-23 11:56:34 +0900

    fix: ext

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{js,ts,tsx}': ['eslint --fix', 'npm test -- --forceExit --runInBand --bail --findRelatedTests', 'git add'],
-  '*.{css}': ['stylelint **/*.scss --fix', 'git add'],
+  '*.{css}': ['stylelint **/*.css --fix', 'git add'],
 };
