dc17e50a93a79881a5c4478081a8ecf3863519dc
hiro0218 <hiro0218@gmail.com>
2021-03-23 12:33:01 +0900

    fix: husky version

--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{js,ts,tsx}': ['eslint --fix', 'npm test -- --forceExit --runInBand --bail --findRelatedTests', 'git add'],
-  '*.{css}': ['stylelint **/*.css --fix', 'git add'],
-};
