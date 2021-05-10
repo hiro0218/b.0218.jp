21f64649ba4d31cd597d7b1b5ed97c6280b4ae3c
hiro0218 <hiro0218@gmail.com>
2021-05-11 03:24:59 +0900

    update: puppeteerを使った実装へ変更

--- prepare/ogp.js
+++ prepare/ogp.js
@@ -1,101 +1,108 @@
 const fs = require('fs-extra');
-const nodeHtmlToImage = require('node-html-to-image');
+// const puppeteer = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
 
-const ogpImages = [];
 const path = {
   src: `${process.cwd()}/dist/posts.json`,
   dist: `${process.cwd()}/public/images/ogp`,
 };
 
-const generateOgpImage = (content) => {
-  let numberOfExecutions = 0;
+const html = `
+<html lang="ja">
+<head>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap');
 
-  nodeHtmlToImage({
-    html: `
-    <html lang="ja">
-    <head>
-      <style>
-        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@900&display=swap');
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
 
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
+    body {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      flex-direction: column;
+      padding: 2vw 8vw;
+      height: 100vh;
+      background: #fff;
+      font-family: "Noto Sans JP", sans-serif;
+      font-feature-settings: "palt";
+      word-wrap: break-word;
+      overflow-wrap: break-word;
+    }
 
-        body {
-          display: flex;
-          align-items: center;
-          justify-content: flex-start;
-          flex-direction: column;
-          padding: 2vw 8vw;
-          height: 100vh;
-          font-family: "Noto Sans JP", sans-serif;
-          font-feature-settings: "palt";
-          word-wrap: break-word;
-          overflow-wrap: break-word;
-        }
+    .title {
+      margin: 1em auto 0;
+      width: 100%;
+      color: #212529;
+      font-size: 5vw;
+      line-height: 1.75;
+      letter-spacing: 0.0125em;
+      text-align: left;
+    }
 
-        .title {
-          margin: 1em auto 0;
-          width: 100%;
-          color: #212529;
-          font-size: 5vw;
-          line-height: 1.75;
-          letter-spacing: 0.0125em;
-          text-align: left;
-        }
+    .url {
+      margin-top: auto;
+      width: 100%;
+      color: #868e96;
+      font-family: sans-serif;
+      font-size: 2vw;
+      text-align: left;
+    }
+  </style>
+</head>
+<body>
+  <h1 class="title">{{title}}</h1>
+  <div class="url">b.0218.jp</div>
+</body>
+</html>
+`;
 
-        .url {
-          margin-top: auto;
-          width: 100%;
-          color: #868e96;
-          font-family: sans-serif;
-          font-size: 2vw;
-          text-align: left;
-        }
-      </style>
-    </head>
-    <body>
-      <h1 class="title">{{title}}</h1>
-      <div class="url">b.0218.jp</div>
-    </body>
-    </html>`,
-    content: content,
-    puppeteerArgs: {
+(async () => {
+  fs.ensureDirSync(path.dist);
+  const posts = fs.readJsonSync(path.src);
+  const length = posts.length;
+
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 4,
+    puppeteerOptions: {
       defaultViewport: {
         width: 1200,
         height: 630,
       },
-      devtools: false,
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu',
+      ],
     },
-    waitUntil: 'networkidle0',
-    beforeScreenshot: () => {
-      console.log(
-        'OGP Image Generated:',
-        content[numberOfExecutions].output.replace(`${path.dist}/`, ''),
-        `(${++numberOfExecutions}/${content.length})`,
-      );
-    },
-  }).then(() => console.log('OGP Image Generated.'));
-};
+  });
 
-(async () => {
-  fs.ensureDirSync(path.dist);
-
-  fs.readJson(path.src)
-    .then((posts) => {
-      posts.forEach((post) => {
-        ogpImages.push({
-          title: post.title,
-          output: `${path.dist}/${post.slug}.png`,
-        });
+  await cluster.task(async ({ page, data: { post, index } }) => {
+    await page
+      .setContent(html.replace('{{title}}', post.title.replace(/</g, '&lt;').replace(/>/g, '&gt;')), {
+        waitUntil: 'networkidle0',
+      })
+      .then(() => {
+        console.log('OGP Image Generated:', `${post.slug}.png`, `(${index + 1}/${length})`);
       });
-    })
-    .then(() => {
-      generateOgpImage(ogpImages);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+    const content = await page.$('body');
+    await content.screenshot({ path: `${path.dist}/${post.slug}.png` });
+    await page.close();
+  });
+
+  posts.forEach((post, index) => {
+    cluster.queue({ post, index });
+  });
+
+  await cluster.idle();
+  await cluster.close();
 })();
