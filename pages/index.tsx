4ca19d4b50e4ea79e0a5047725d4718753412dbf
hiro0218 <hiro0218@gmail.com>
2021-05-13 21:20:15 +0900

    remove: Qiita,Zenn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import fs from 'fs-extra';
 import { GetStaticProps } from 'next';
 import path from 'path';
-import Parser from 'rss-parser';
 
 import Heading from '@/components/Heading';
 import { MenuList, MenuListItem } from '@/components/layout/MenuList';
@@ -9,20 +8,12 @@ import PageContainer from '@/components/layout/PageContainer';
 import LinkCard from '@/components/LinkCard';
 import { Post as PropsPost } from '@/types/source';
 
-interface Feed {
-  title: string;
-  link: string;
-  isoDate: string;
-}
-
 interface Props {
   recentPosts: Array<PropsPost>;
   updatesPosts: Array<PropsPost>;
-  zennPosts: Array<Feed>;
-  qiitaPosts: Array<Feed>;
 }
 
-const Home = ({ recentPosts, updatesPosts, zennPosts, qiitaPosts }: Props) => {
+const Home = ({ recentPosts, updatesPosts }: Props) => {
   return (
     <>
       <PageContainer>
@@ -55,38 +46,6 @@ const Home = ({ recentPosts, updatesPosts, zennPosts, qiitaPosts }: Props) => {
               ))}
             </MenuList>
           </section>
-
-          <section className="p-home-section">
-            <header>
-              <Heading tagName={'h2'} text={'Qiita: Recent Articles'} isWeightNormal={true} />
-            </header>
-            <MenuList className="p-home-section__contents">
-              {qiitaPosts.map(
-                (post, index) =>
-                  index < 5 && (
-                    <MenuListItem key={index}>
-                      <LinkCard link={post.link} title={post.title} date={post.isoDate} target={true} />
-                    </MenuListItem>
-                  ),
-              )}
-            </MenuList>
-          </section>
-
-          <section className="p-home-section">
-            <header>
-              <Heading tagName={'h2'} text={'Zenn: Recent Articles'} isWeightNormal={true} />
-            </header>
-            <MenuList className="p-home-section__contents">
-              {zennPosts.map(
-                (post, index) =>
-                  index < 5 && (
-                    <MenuListItem key={index}>
-                      <LinkCard link={post.link} title={post.title} date={post.isoDate} target={true} />
-                    </MenuListItem>
-                  ),
-              )}
-            </MenuList>
-          </section>
         </section>
       </PageContainer>
     </>
@@ -99,13 +58,6 @@ export const getStaticProps: GetStaticProps = async () => {
   const postsPath = path.join(process.cwd(), 'dist/posts.json');
   const posts: Array<PropsPost> = fs.readJsonSync(postsPath);
 
-  // 外部サービス
-  const parser = new Parser();
-  const feedZenn = await parser.parseURL('https://zenn.dev/hiro/feed');
-  const feedQiita = await parser.parseURL('https://qiita.com/hiro0218/feed.atom');
-  const zennPosts = feedZenn.items;
-  const qiitaPosts = feedQiita.items;
-
   return {
     props: {
       recentPosts: posts.filter((_, i) => i < 5),
@@ -115,8 +67,6 @@ export const getStaticProps: GetStaticProps = async () => {
         })
         .filter((value) => value.date < value.updated)
         .filter((_, i) => i < 5),
-      zennPosts,
-      qiitaPosts,
     },
   };
 };
