91efdeb719d76cf2c04521d5b3bc2abd7a2e037e
hiro0218 <hiro0218@gmail.com>
2021-11-01 21:40:47 +0900

    fix: 定義場所を修正

--- components/layout/Stack.tsx
+++ components/layout/Stack.tsx
@@ -16,22 +16,26 @@ export interface StackProps {
   children?: React.ReactNode;
 }
 
+const Root = styled.div<StackProps>`
+  display: flex;
+  gap: ${(props) => props.gap || ''};
+  flex-direction: ${(props) => props.direction || ''};
+  align-items: ${(props) => props.align || ''};
+  justify-content: ${(props) => props.justify || ''};
+  flex-wrap: ${(props) => props.wrap || ''};
+  flex-basis: ${(props) => props.basis || ''};
+  flex-grow: ${(props) => props.grow || ''};
+  flex-shrink: ${(props) => props.shrink || ''};
+`;
+
 export const Stack = memo(function Stack(props: StackProps) {
   const { children, ...others } = props;
 
-  const Root = styled.div<StackProps>`
-    display: flex;
-    gap: ${(props) => props.gap || ''};
-    flex-direction: ${(props) => props.direction || ''};
-    align-items: ${(props) => props.align || ''};
-    justify-content: ${(props) => props.justify || ''};
-    flex-wrap: ${(props) => props.wrap || ''};
-    flex-basis: ${(props) => props.basis || ''};
-    flex-grow: ${(props) => props.grow || ''};
-    flex-shrink: ${(props) => props.shrink || ''};
-  `;
-
-  return <Root {...others}>{children}</Root>;
+  return (
+    <Root className="l-stack" {...others}>
+      {children}
+    </Root>
+  );
 }) as NamedExoticComponent<StackProps> & {
   Item: typeof Item;
 };
