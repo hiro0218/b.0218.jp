18efe3f149cb86a18aa159f8528d5bac6b74de94
hiro0218 <hiro0218@gmail.com>
2021-11-02 22:31:49 +0900

    add: storybook/addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
     'storybook-addon-next-router',
     'storybook-addon-turbo-build',
   ],
