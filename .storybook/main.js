08046d2836ae908dbc0e11b7320fe834c67126dd
hiro0218 <hiro0218@gmail.com>
2021-11-04 22:30:05 +0900

    fix: emotionが動作するよう修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -15,6 +16,27 @@ module.exports = {
       '@': path.resolve(__dirname, '../'),
     };
 
-    return config;
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+
+          /**
+           * Map Emotion 10 libraries to Emotion 11 libraries.
+           *
+           * Otherwise Storybook fails to compile with "Module not found: Error: Can't resolve '@emotion/styled/base'", etc.
+           * It wasn't necessary to do this until we imported React component using "@emotion/styled".
+           * This issue is probably caused because Storybook uses Emotion 10 while we have Emotion 11 used by the Next.js app.
+           *
+           * @see https://github.com/storybookjs/storybook/issues/13277#issuecomment-751747964
+           */
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+      },
+    };
   },
 };
