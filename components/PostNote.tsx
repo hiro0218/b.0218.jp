88bea2942d591e2c96e9ca64f4482c371b980d94
hiro0218 <hiro0218@gmail.com>
2021-11-04 17:46:15 +0900

    add: Note Component

---
+++ components/PostNote.tsx
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+import { FC } from 'react';
+
+import { Post } from '@/types/source';
+
+type Props = Pick<Post, 'note'>;
+
+const styleNote = css`
+  display: flex;
+  padding: calc(var(--margin-base) * 0.6) calc(var(--margin-base) * 0.8);
+  border: 2px solid var(--gray-1);
+  border-radius: 0.25rem;
+  line-height: 1.8;
+
+  &::before {
+    content: 'Note:';
+    margin-right: 0.25em;
+    font-weight: bold;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+`;
+
+const PostNote: FC<Props> = ({ note }) => {
+  return <div css={styleNote} dangerouslySetInnerHTML={{ __html: note }} />;
+};
+
+export default PostNote;
