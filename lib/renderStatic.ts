d08bdc13d3b642cd942a86b53e54a1959d8e5f22
hiro0218 <hiro0218@gmail.com>
2021-11-09 01:43:45 +0900

    chore(update): emotion/server

--- lib/renderStatic.ts
+++
@@ -1,9 +0,0 @@
-import { cache } from '@emotion/css';
-import createEmotionServer from '@emotion/server/create-instance';
-
-export const renderStatic = async (html: string) => {
-  const { extractCritical } = createEmotionServer(cache);
-  const { ids, css } = extractCritical(html);
-
-  return { html, ids, css };
-};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,20 @@
+import { extractCritical } from '@emotion/server';
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
 import { GOOGLE_ADSENSE } from '@/components/Adsense';
 import { GA_TRACKING_ID } from '@/lib/gtag';
-import { renderStatic } from '@/lib/renderStatic';
 
 class SampleDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const page = await ctx.renderPage();
-    const { css, ids } = await renderStatic(page.html);
     const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
 
     return {
       ...initialProps,
       styles: (
         <>
           {initialProps.styles}
-          <style data-emotion={`css ${ids.join(' ')}`} dangerouslySetInnerHTML={{ __html: css }} />
+          <style data-emotion={`css ${styles.ids.join(' ')}`} dangerouslySetInnerHTML={{ __html: styles.css }} />
         </>
       ),
     };
