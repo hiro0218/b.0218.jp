aaca61b51f58af300d7479373cea9ac203cd661d
hiro0218 <hiro0218@gmail.com>
2021-11-15 23:19:38 +0900

    change: css to emotion

--- components/TheFooter.tsx
+++ components/TheFooter.tsx
@@ -1,35 +1,82 @@
+import styled from '@emotion/styled';
 import Link from 'next/link';
 import { FC } from 'react';
 
+const Footer = styled.footer`
+  margin-top: calc(var(--margin-base) * 4);
+  padding: 3rem 0;
+  background: var(--bg-color--lighter);
+  color: var(--color-text--light);
+  line-height: 1;
+  text-align: center;
+
+  a {
+    color: inherit;
+
+    &:hover {
+      text-decoration-line: underline;
+    }
+  }
+`;
+
+const FooterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--container-width);
+  margin: 0 auto;
+  transition: padding 0.1s ease-in-out;
+
+  @media (max-width: 959px) {
+    padding: 0 5vw;
+  }
+`;
+
+const FooterMeneList = styled.ul`
+  display: inline-flex;
+  padding: 0;
+  list-style: none;
+`;
+
+const FooterMeneListItem = styled.li`
+  & + & {
+    margin-left: 1em;
+  }
+`;
+
+const FooterCopyright = styled.div`
+  small {
+    font-size: 1em;
+  }
+`;
+
 const TheFooter: FC = () => {
   return (
-    <footer className="pj-footer">
-      <div className="pj-footer-container">
-        <div className="pj-footer-menu">
-          <ul className="pj-footer-menu__list">
-            <li className="pj-footer-menu__item">
-              <Link href="/" prefetch={false}>
-                <a className="pj-footer__link">home</a>
-              </Link>
-            </li>
-            <li className="pj-footer-menu__item">
-              <Link href="/about" prefetch={false}>
-                <a className="pj-footer__link">about</a>
-              </Link>
-            </li>
-            <li className="pj-footer-menu__item">
-              <Link href="/archive" prefetch={false}>
-                <a className="pj-footer__link">archive</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <Footer>
+      <FooterContainer>
+        <FooterMeneList>
+          <FooterMeneListItem>
+            <Link href="/" prefetch={false}>
+              <a>home</a>
+            </Link>
+          </FooterMeneListItem>
+          <FooterMeneListItem>
+            <Link href="/about" prefetch={false}>
+              <a>about</a>
+            </Link>
+          </FooterMeneListItem>
+          <FooterMeneListItem>
+            <Link href="/archive" prefetch={false}>
+              <a>archive</a>
+            </Link>
+          </FooterMeneListItem>
+        </FooterMeneList>
 
-        <div className="pj-footer-copyright">
-          <small className="pj-footer-copyright__text">©&nbsp;hiro</small>
-        </div>
-      </div>
-    </footer>
+        <FooterCopyright>
+          <small>©&nbsp;hiro</small>
+        </FooterCopyright>
+      </FooterContainer>
+    </Footer>
   );
 };
 
