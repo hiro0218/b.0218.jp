f43fa83e46544287b61c328e0b6384e68c239508
hiro0218 <hiro0218@gmail.com>
2021-11-19 00:36:11 +0900

    remove: avator

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -90,7 +90,6 @@ const TheHeader: FC = () => {
         <div className="pj-header-container">
           <Link href="/" prefetch={false}>
             <a className="pj-header__logo">
-              <img src="/hiro0218@100x100.webp" className="pj-header__avatar" alt="avatar" height="32" width="32" />
               <img src="/logo.svg" width="80" alt={SITE.NAME} />
             </a>
           </Link>
