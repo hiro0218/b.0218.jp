051b2965040728e8dd00aefd31d74ba520ed146a
hiro0218 <hiro0218@gmail.com>
2021-11-19 15:33:25 +0900

    Revert "fix: styled -> css"
    This reverts commit 37cb6966604d1a184117f4cbafc0663fae3b1655.

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import React, { CSSProperties, memo } from 'react';
 
 type divProps = JSX.IntrinsicElements['div'];
@@ -7,7 +7,7 @@ export interface ContainerProps extends divProps {
   children?: React.ReactNode;
 }
 
-const styles = css`
+const Root = styled.div<ContainerProps>`
   max-width: var(--container-width);
   margin: var(--margin-base) auto 0;
   transition: padding 0.1s ease-in-out;
@@ -21,8 +21,8 @@ export const Container = memo(function Container(props: ContainerProps) {
   const { children, ...others } = props;
 
   return (
-    <div css={styles} className="l-container" {...others}>
+    <Root className="l-container" {...others}>
       {children}
-    </div>
+    </Root>
   );
 });
