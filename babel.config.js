616a1601b7462a480c77be8bc3ae7c930210040c
hiro0218 <hiro0218@gmail.com>
2021-12-03 16:54:24 +0900

    fix: modules,useBuiltIns設定を追加

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
+/** @type {import('@babel/core').TransformOptions} */
 module.exports = {
   presets: [
     [
       'next/babel',
       {
+        'preset-env': {
+          modules: false,
+          useBuiltIns: 'usage',
+        },
         'preset-react': {
           runtime: 'automatic',
           importSource: '@emotion/react',
