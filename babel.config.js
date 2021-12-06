be3986a88237ffba545d688235e7eee6c8488039
hiro0218 <hiro0218@gmail.com>
2021-12-06 11:38:32 +0900

    fix(add): corejs

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'preset-env': {
           modules: false,
           useBuiltIns: 'usage',
+          corejs: 3,
         },
         'preset-react': {
           runtime: 'automatic',
