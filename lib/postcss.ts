8b12a8109adf36f4e8334527112d4749ded86583
hiro0218 <hiro0218@gmail.com>
2022-01-07 09:51:43 +0900

    Revert "update: apply postcss"
    This reverts commit 0cbc89dbb8af8b6e3a5478ca8ae5723d5f627016.

--- lib/postcss.ts
+++
@@ -1,9 +0,0 @@
-import postcss from 'postcss';
-
-export async function processPostCSS(css: string) {
-  return postcss()
-    .process(css)
-    .then((result) => {
-      return result.css;
-    });
-}
