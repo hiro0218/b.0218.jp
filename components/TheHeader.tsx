dfe485976404afe032eac39c87912c72fca8f74f
hiro0218 <hiro0218@gmail.com>
2022-01-07 22:44:14 +0900

    fix: css -> emotion

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC, useCallback, useEffect, useRef, useState } from 'react';
@@ -6,6 +7,7 @@ import ReactModal from 'react-modal';
 
 import { Logo } from '@/components/Logo';
 import Search from '@/components/Search';
+import { mobile } from '@/lib/mediaQuery';
 
 ReactModal.setAppElement('#__next');
 const customStyles = {
@@ -86,7 +88,7 @@ const TheHeader: FC = () => {
 
   return (
     <>
-      <header ref={refHeader} className="pj-header">
+      <header ref={refHeader} css={cssHeader} className="pj-header">
         <div className="pj-header-container">
           <Link href="/" prefetch={false}>
             <a className="pj-header__logo">
@@ -115,3 +117,72 @@ const TheHeader: FC = () => {
 };
 
 export default TheHeader;
+
+const cssHeader = css`
+  position: fixed;
+  z-index: var(--zIndex-header);
+  top: 0;
+  right: 0;
+  left: 0;
+  height: var(--header-height);
+  margin: 0 auto;
+  transition: transform 0.25s ease;
+  pointer-events: none;
+  will-change: transform;
+
+  &.is-unpin {
+    transform: translateY(calc(var(--header-height) * -1));
+    box-shadow: none;
+  }
+
+  .pj-header-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: var(--container-width);
+    height: 100%;
+    margin: 0 auto;
+    transition: padding 0.1s ease-in-out;
+
+    ${mobile} {
+      padding: 0 5vw;
+    }
+  }
+
+  .pj-header__logo {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    color: var(--color-text);
+    pointer-events: auto;
+  }
+
+  .pj-header-search {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    height: 3rem;
+    transition: background 0.2s ease;
+    border: none;
+    border-radius: 100%;
+    background: none;
+    cursor: pointer;
+    pointer-events: auto;
+
+    &:hover,
+    &:focus {
+      background: var(--bg-color--lighter);
+    }
+  }
+
+  .pj-header-search__icon {
+    display: flex;
+
+    & svg {
+      width: 1.25rem;
+      height: 1.25rem;
+      color: var(--color-text--light);
+    }
+  }
+`;
