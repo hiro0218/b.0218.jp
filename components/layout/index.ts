e1c2987cd2cabedb7b1cabb610c1b509b29f5c21
hiro0218 <hiro0218@gmail.com>
2022-01-17 18:49:28 +0900

    delete: lower name directory

--- components/layout/index.ts
+++
@@ -1,6 +0,0 @@
-import { Columns } from './Columns';
-import { Container } from './Container';
-import { PageContentContainer } from './PageContentContainer';
-import { Stack } from './Stack';
-
-export { Columns, Container, PageContentContainer, Stack };
