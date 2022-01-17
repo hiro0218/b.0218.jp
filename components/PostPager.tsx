ea32e26de1aa2e38989fb033d5b3d9b0e3c2f091
hiro0218 <hiro0218@gmail.com>
2022-01-17 14:42:38 +0900

    fix: css -> emotion

--- components/PostPager.tsx
+++ components/PostPager.tsx
@@ -1,7 +1,9 @@
+import styled from '@emotion/styled';
 import Link from 'next/link';
 import { FC } from 'react';
 import { HiOutlineArrowLeft, HiOutlineArrowRight } from 'react-icons/hi';
 
+import { mobile } from '@/lib/mediaQuery';
 import { NextPrevPost } from '@/types/source';
 
 type Props = {
@@ -11,39 +13,99 @@ type Props = {
 
 const PostPager: FC<Props> = ({ next, prev }) => {
   return (
-    <nav className="c-pager">
+    <PagerRoot>
       {Object.keys(prev).length !== 0 && (
-        <Link href={`${prev.slug}.html`} prefetch={false}>
-          <a className="c-pager__item--prev">
-            <div className="c-pager-icon--prev">
-              <div className="c-pager-icon__arrow">
-                <HiOutlineArrowLeft />
-              </div>
-            </div>
-            <div className="c-pager-main">
-              <div className="c-pager__label">Prev</div>
-              <div className="c-pager__title">{prev.title}</div>
-            </div>
-          </a>
+        <Link href={`${prev.slug}.html`} prefetch={false} passHref>
+          <PagerAnchorItem>
+            <PagerIcon>
+              <HiOutlineArrowLeft />
+            </PagerIcon>
+            <PagerMain>
+              <div className="label">Prev</div>
+              <div className="title">{prev.title}</div>
+            </PagerMain>
+          </PagerAnchorItem>
         </Link>
       )}
       {Object.keys(next).length !== 0 && (
-        <Link href={`${next.slug}.html`} prefetch={false}>
-          <a className="c-pager__item--next">
-            <div className="c-pager-main">
-              <div className="c-pager__label">Next</div>
-              <div className="c-pager__title">{next.title}</div>
-            </div>
-            <div className="c-pager-icon--next">
-              <div className="c-pager-icon__arrow">
-                <HiOutlineArrowRight />
-              </div>
-            </div>
-          </a>
+        <Link href={`${next.slug}.html`} prefetch={false} passHref>
+          <PagerAnchorItem>
+            <PagerMain>
+              <div className="label">Next</div>
+              <div className="title">{next.title}</div>
+            </PagerMain>
+            <PagerIcon>
+              <HiOutlineArrowRight />
+            </PagerIcon>
+          </PagerAnchorItem>
         </Link>
       )}
-    </nav>
+    </PagerRoot>
   );
 };
 
 export default PostPager;
+
+const PagerRoot = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  font-size: var(--font-size-sm);
+`;
+
+const PagerAnchorItem = styled.a`
+  display: flex;
+  align-items: flex-start;
+  gap: 0.75rem;
+  width: 50%;
+  padding: 1rem;
+  transition: background 0.2s ease, box-shadow 0.4s ease;
+  border-radius: 0.25em;
+  color: var(--color-text);
+  line-height: 1.8;
+  word-break: break-all;
+
+  ${mobile} {
+    width: 100%;
+  }
+
+  &:hover {
+    background: var(--bg-color--lighter);
+    box-shadow: 0 0 0 0.25rem var(--bg-color--lighter);
+  }
+
+  &:only-child {
+    flex-grow: 1;
+  }
+
+  &:nth-child(2) {
+    text-align: right;
+  }
+`;
+
+const PagerMain = styled.div`
+  flex-grow: 1;
+
+  .label {
+    color: var(--color-text--lighter);
+    font-weight: bold;
+    line-height: 1;
+  }
+
+  .title {
+    margin-top: 0.5rem;
+    color: var(--color-text);
+  }
+`;
+
+const PagerIcon = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  color: var(--color-text--lighter);
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+`;
