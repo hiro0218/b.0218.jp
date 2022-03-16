6c50d23a0348bdbf49a8e4c8ff964bb8ba53ac3c
hiro0218 <hiro0218@gmail.com>
2022-03-16 22:47:57 +0900

    fix: 不要な記述を削除

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { css, keyframes } from '@emotion/react';
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
 import { FC, useEffect, useRef, useState } from 'react';
