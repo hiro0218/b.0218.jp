c0b88647a06ee71eb673cd1a8908ec01d819f8c1
hiro0218 <hiro0218@gmail.com>
2022-03-23 23:37:56 +0900

    fix: CSS -> Emotion

--- components/CssBaseline/CssBaseline.tsx
+++ components/CssBaseline/CssBaseline.tsx
@@ -3,11 +3,13 @@ import { css, Global } from '@emotion/react';
 import elementStyle from './Elements';
 import resetStyle from './Generic';
 import variableStyle from './Settings';
+import vendorStyle from './Vendor';
 
 const globalStyle = css`
   ${variableStyle}
   ${resetStyle}
   ${elementStyle}
+  ${vendorStyle}
 `;
 
 const CssBaseline = () => {
