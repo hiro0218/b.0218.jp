3dcb12d1d086c63a1000a77553741a398314ec9a
hiro0218 <hiro0218@gmail.com>
2022-04-14 21:12:14 +0900

    fix: dialog animation

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,3 +1,4 @@
+import { css, jsx, keyframes } from '@emotion/react'
 import styled from '@emotion/styled';
 import dynamic from 'next/dynamic'
 import Link from 'next/link';
@@ -114,6 +115,34 @@ export const TheHeader: FC = () => {
   );
 };
 
+const slideIn = keyframes`
+  0% {
+    transform: translateY(400px);
+    animation-timing-function: ease-out;
+  }
+  60% {
+    transform: translateY(-30px);
+    animation-timing-function: ease-in;
+  }
+  80% {
+    transform: translateY(10px);
+    animation-timing-function: ease-out;
+  }
+  100% {
+    transform: translateY(0px);
+    animation-timing-function: ease-in;
+  }
+`
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
 const HeaderRoot = styled.header`
   position: fixed;
   z-index: var(--zIndex-header);
@@ -185,8 +214,8 @@ const HeaderSearchButton = styled.button`
 
 const Dialog = styled.dialog`
   &[open] {
-    top: 10vh;
     padding: 0;
+    animation: ${fadeIn} 0.4s, ${slideIn} 0.4s linear;
     border: none;
   }
 `
