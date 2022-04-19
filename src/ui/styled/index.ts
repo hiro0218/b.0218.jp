46f1b6a6cfdeb20d563b4d7f4cd5d6eac7c63bcd
hiro0218 <hiro0218@gmail.com>
2022-04-20 00:29:40 +0900

    add: ui/styled

---
+++ src/ui/styled/index.ts
@@ -0,0 +1,24 @@
+import {
+  CacheProvider,
+  css,
+  EmotionCache,
+  Global,
+  keyframes,
+  SerializedStyles,
+  Theme,
+  ThemeProvider,
+  useTheme,
+} from '@emotion/react'
+import styled from '@emotion/styled'
+
+export {
+  css,
+  Global,
+  keyframes,
+  CacheProvider,
+  ThemeProvider,
+  useTheme,
+  styled,
+}
+
+export type { EmotionCache, Theme, SerializedStyles }
