9e95d1d685744a08e8cfb40bab9b7a369973a917
hiro0218 <hiro0218@gmail.com>
2022-05-03 13:36:43 +0900

    fix(pref): setContentを事前にするよう修正

--- src/build/ogp/index.ts
+++ src/build/ogp/index.ts
@@ -1,5 +1,5 @@
 import { loadDefaultJapaneseParser } from 'budoux';
-import fs from 'fs-extra';
+import { ensureDirSync, readFileSync } from 'fs-extra';
 import { chromium } from 'playwright';
 const parser = loadDefaultJapaneseParser();
 
@@ -9,10 +9,10 @@ const path = {
   dist: `${process.cwd()}/public/images/ogp`,
 };
 
-const html = fs.readFileSync(`${process.cwd()}/src/build/ogp/template.html`, 'utf-8');
+const template = readFileSync(`${process.cwd()}/src/build/ogp/template.html`, 'utf-8');
 
 (async () => {
-  fs.ensureDirSync(path.dist);
+  ensureDirSync(path.dist);
   const posts = getPostsJson();
   const length = posts.length;
   let browser = null;
@@ -20,32 +20,32 @@ const html = fs.readFileSync(`${process.cwd()}/src/build/ogp/template.html`, 'ut
   try {
     browser = await chromium.launch();
     const page = await browser.newPage();
-
+    await page.setContent(template, {
+      waitUntil: 'domcontentloaded'
+    })
     await page.setViewportSize({ width: 1200, height: 630 });
 
     let index = 1;
     for (const { title, slug } of posts) {
-      await page
-        .setContent(
-          html.replace('{{title}}', parser.translateHTMLString(title.replace(/</g, '&lt;').replace(/>/g, '&gt;'))),
-          {
-            waitUntil: 'domcontentloaded',
-          },
-        )
-        .then(() => {
-          if (index === 1 || index % 100 === 0 || index === length) {
-            console.log('Generating OGP Images', `(${index}/${length})`);
-          }
-        });
-      await page.evaluate(async () => {
-        await Promise.all([document.fonts.ready]);
-      });
+      const pageTitle = parser.translateHTMLString(title.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
+
+      await page.evaluate(async (pageTitle: string) => {
+        await Promise.all([
+          document.getElementById('title').innerHTML = pageTitle,
+          document.fonts.ready,
+        ]);
+      }, pageTitle);
 
       const content = await page.$('body');
       await content.screenshot({
         fullPage: false,
         path: `${path.dist}/${slug}.png`,
+      }).then(() => {
+        if (index === 1 || index % 100 === 0 || index === length) {
+          console.log('Generating OGP Images', `(${index}/${length})`);
+        }
       });
+
       index++;
     }
   } catch (err) {
