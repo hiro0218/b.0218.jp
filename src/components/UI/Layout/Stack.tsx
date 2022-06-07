403a71d7d732ef1a4607d6e96abc15c484128c2f
hiro0218 <hiro0218@gmail.com>
2022-06-07 22:21:16 +0900

    add: Stack Component

---
+++ src/components/UI/Layout/Stack.tsx
@@ -0,0 +1,25 @@
+import { AriaRole, CSSProperties, memo, ReactNode } from 'react';
+
+import { styled } from '@/ui/styled';
+
+type Props = {
+  space?: CSSProperties['margin'];
+  role?: AriaRole;
+  children: ReactNode;
+}
+
+const StackRoot = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  & > * + * {
+    margin-block-start: ${props => props.space || 'var(--space-md)'};
+  }
+`;
+
+const Stack = memo(function Stack({ children, ...props }: Props) {
+  return (<StackRoot {...props}>{children}</StackRoot>)
+});
+
+export default Stack;
