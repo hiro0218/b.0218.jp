116c66713e2b9d60a5005f8501f69a1905493a89
hiro0218 <hiro0218@gmail.com>
2022-06-20 22:29:36 +0900

    fix: lockしないようにする

--- src/components/UI/Search/useDialog.ts
+++ src/components/UI/Search/useDialog.ts
@@ -2,22 +2,17 @@ import { useCallback, useRef } from 'react';
 
 export const useModal = () => {
   const ref = useRef<HTMLDialogElement>(null);
-  const refStyleOverflow = useRef<CSSStyleDeclaration['overflow']>(
-    typeof window !== 'undefined' ? window.getComputedStyle(document.body).overflow : '',
-  );
 
   const openDialog = useCallback(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     ref.current?.showModal();
-    document.body.style.overflow = 'hidden';
   }, []);
 
   const closeDialog = useCallback(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     ref.current?.close();
-    document.body.style.overflow = refStyleOverflow.current;
   }, []);
 
   return { ref, openDialog, closeDialog };
