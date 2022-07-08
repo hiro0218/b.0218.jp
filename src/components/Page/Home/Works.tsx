bd0b7271cda932e1e60f428b65303f80f31536ca
hiro0218 <hiro0218@gmail.com>
2022-07-08 18:37:31 +0900

    init: works component

---
+++ src/components/Page/Home/Works.tsx
@@ -0,0 +1,114 @@
+import { GoRepoForked, GoStar } from 'react-icons/go';
+
+import Heading from '@/components/UI/Heading';
+import LinkCard from '@/components/UI/LinkCard';
+import { URL } from '@/constant';
+import { mobile } from '@/lib/mediaQuery';
+import { GithubPinnedItems } from '@/types/source';
+import { styled } from '@/ui/styled';
+
+type Props = {
+  items: Array<GithubPinnedItems>;
+};
+
+export const Works = ({ items }: Props) => (
+  <section>
+    <Heading
+      tagName="h2"
+      text={'Works'}
+      textSide={
+        <ViewAll href={URL.GITHUB} target="_blank">
+          View All
+        </ViewAll>
+      }
+      textSub={'GitHub Repositories'}
+      isWeightNormal={false}
+    />
+    <Container>
+      {items.map((item, index) => (
+        <LinkCard
+          key={index}
+          link={item.homepageUrl || item.url}
+          title={item.name}
+          excerpt={
+            <Description
+              description={item.description}
+              forkCount={item.forkCount}
+              stargazerCount={item.stargazerCount}
+              languages={item.languages}
+            />
+          }
+          target={true}
+        />
+      ))}
+    </Container>
+  </section>
+);
+
+const Description = ({ description, forkCount, stargazerCount, languages }: Partial<GithubPinnedItems>) => (
+  <DescriptionContainer>
+    <span>{description}</span>
+    <StatusList>
+      <StatusListItem>
+        <StatusLanguageCircle color={languages.color} aria-hidden="true" />
+        {languages.name}
+      </StatusListItem>
+      <StatusListItem>
+        <GoStar size={16} />
+        {stargazerCount}
+      </StatusListItem>
+      <StatusListItem>
+        <GoRepoForked size={16} />
+        {forkCount}
+      </StatusListItem>
+    </StatusList>
+  </DescriptionContainer>
+);
+
+const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: calc(var(--margin-base) * 0.25);
+`;
+
+const StatusList = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const StatusListItem = styled.li`
+  display: inline-flex;
+  align-items: center;
+  font-size: var(--font-size-sm);
+
+  & + & {
+    margin-left: 1em;
+  }
+`;
+
+const StatusLanguageCircle = styled.span<{ color: string }>`
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+  margin-right: 0.25em;
+  border-radius: 50%;
+  background-color: ${({ color }) => color};
+`;
+
+const ViewAll = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(50% - var(--space-md)), 1fr));
+  gap: var(--space-md);
+  margin-top: var(--space-md);
+
+  ${mobile} {
+    grid-template-columns: 1fr;
+    gap: var(--space-sm);
+  }
+`;
--- src/components/Page/Home/index.ts
+++ src/components/Page/Home/index.ts
@@ -1,1 +1,2 @@
 export { Hero } from './Hero';
+export { Works } from './Works';
