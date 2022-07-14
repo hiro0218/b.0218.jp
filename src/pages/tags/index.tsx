52312a422ef73f243d54a856cd7a209528617c38
hiro0218 <hiro0218@gmail.com>
2022-07-15 00:18:25 +0900

    add: title

--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Head from 'next/head';
 
 import { Columns, PageContentContainer } from '@/components/UI/Layout';
 import PostTag, { PostTagGridContainer, Props as PostTagProps } from '@/components/UI/Tag';
 import { Title } from '@/components/UI/Title';
+import { SITE } from '@/constant';
 import { getTermWithCount } from '@/lib/posts';
 
 type TermProps = {
@@ -11,14 +13,19 @@ type TermProps = {
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const Tags: NextPage<Props> = ({ tags }) => (
-  <PageContentContainer>
-    <Title heading="Tags" />
-    <Columns>
-      <PostTagGridContainer>
-        <PostTag tags={tags} />
-      </PostTagGridContainer>
-    </Columns>
-  </PageContentContainer>
+  <>
+    <Head>
+      <title key="title">{`Tags - ${SITE.NAME}`}</title>
+    </Head>
+    <PageContentContainer>
+      <Title heading="Tags" />
+      <Columns>
+        <PostTagGridContainer>
+          <PostTag tags={tags} />
+        </PostTagGridContainer>
+      </Columns>
+    </PageContentContainer>
+  </>
 );
 
 export default Tags;
