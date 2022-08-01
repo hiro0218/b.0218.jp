9cbf80f8d650d59456df1322b58fd2beb6bea32c
hiro0218 <hiro0218@gmail.com>
2022-08-02 00:34:41 +0900

    chore

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,14 +5,14 @@ const toPath = (_path) => path.join(process.cwd(), _path);
 function getPackageDir(filepath) {
   let currDir = path.dirname(require.resolve(filepath));
   while (true) {
-      if (fs.existsSync(path.join(currDir, 'package.json'))) {
-          return currDir;
-      }
-      const { dir, root } = path.parse(currDir);
-      if (dir === root) {
-          throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
-      }
-      currDir = dir;
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+    }
+    currDir = dir;
   }
 }
 
@@ -23,14 +23,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
     'storybook-addon-next-router',
     'storybook-addon-turbo-build',
   ],
