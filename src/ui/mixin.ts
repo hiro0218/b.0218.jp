e30a94e57d91a2ccb9f2620326efc18747a839a5
hiro0218 <hiro0218@gmail.com>
2022-08-17 16:38:13 +0900

    refactor

--- src/ui/mixin.ts
+++ src/ui/mixin.ts
@@ -25,7 +25,7 @@ export const showHoverBackground = css`
     height: 50%;
     margin: auto;
     transform: scale(0.1);
-    transition: transform 0.4s ${easing.easeOutBack}, opacity 0.4s;
+    transition: transform 0.4s ${easing.easeOutBack};
     border-radius: 0.25rem;
     opacity: 0;
     background-image: linear-gradient(120deg, var(--backgrounds-2), var(--component-backgrounds-4));
