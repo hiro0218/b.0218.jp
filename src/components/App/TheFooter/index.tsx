42c03688b6411aa72f5973c9256dbca50a89a11f
hiro0218 <hiro0218@gmail.com>
2022-08-18 23:46:21 +0900

    fix: リンク一覧を配列に変更

--- src/components/App/TheFooter/index.tsx
+++ src/components/App/TheFooter/index.tsx
@@ -2,30 +2,24 @@ import { Anchor } from '@/components/UI/Anchor';
 import { mobile } from '@/lib/mediaQuery';
 import { styled } from '@/ui/styled';
 
+const Links = [
+  { title: 'about', href: '/about' },
+  { title: 'works', href: '/works' },
+  { title: 'tags', href: '/tags' },
+  { title: 'archive', href: '/archive' },
+];
+
 const TheFooter = () => (
   <Footer>
     <Container>
       <List role="list">
-        <ListItem role="listitem">
-          <Anchor href="/about" prefetch={false} passHref>
-            <a>about</a>
-          </Anchor>
-        </ListItem>
-        <ListItem role="listitem">
-          <Anchor href="/archive" prefetch={false} passHref>
-            <a>archive</a>
-          </Anchor>
-        </ListItem>
-        <ListItem role="listitem">
-          <Anchor href="/tags" prefetch={false} passHref>
-            <a>tags</a>
-          </Anchor>
-        </ListItem>
-        <ListItem role="listitem">
-          <Anchor href="/works" prefetch={false} passHref>
-            <a>works</a>
-          </Anchor>
-        </ListItem>
+        {Links.map(({ title, href }, index) => {
+          return (
+            <ListItem role="listitem" key={index}>
+              <Anchor href={href}>{title}</Anchor>
+            </ListItem>
+          );
+        })}
       </List>
       <small>© hiro</small>
     </Container>
