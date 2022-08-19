672f540a361a31e05e8ab9fb40e55b7c81721d86
hiro0218 <hiro0218@gmail.com>
2022-08-19 22:49:03 +0900

    replace: Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 
+import { Hero } from '@/components/Page/Home/Hero';
 import Heading from '@/components/UI/Heading';
 import { Columns, PageContentContainer, Stack } from '@/components/UI/Layout';
 import LinkCard from '@/components/UI/LinkCard';
 import PostTag, { PostTagGridContainer, Props as PostTagProps } from '@/components/UI/Tag';
-import { Title } from '@/components/UI/Title';
 import { getPostsListJson, getTermWithCount } from '@/lib/posts';
 import { Post as PropsPost } from '@/types/source';
 
@@ -39,10 +39,7 @@ const Home: NextPage<Props> = ({ recentPosts, updatesPosts, tags }) => {
       />
 
       <PageContentContainer>
-        <Title
-          heading="Hello, I'm hiro."
-          paragraph="I was a web backend developer and native app developer. Currently I am a web frontend developer."
-        />
+        <Hero />
 
         <Stack>
           <Heading tagName="h2" text="Articles" />
