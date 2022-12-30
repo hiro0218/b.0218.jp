ce5a1c3adbdfd3481ea1254916a79c9e965865bc
hiro0218 <hiro0218@gmail.com>
2022-12-30 18:47:36 +0900

    fix: hooks 分離

---
+++ src/components/App/TheHeader/useScrollHandler.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+import { theme } from '@/ui/themes';
+
+export const useScrollHandler = () => {
+  const [isHeaderShown, setIsHeaderShown] = useState(true);
+
+  useEffect(() => {
+    const headerHeight = theme.components.header.height;
+    let ticking = false;
+    let lastScrollY = 0;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (!ticking) {
+        requestAnimationFrame(() => {
+          ticking = false;
+
+          // ヘッダーの高さを超えた場合
+          if (currentScrollY >= headerHeight) {
+            setIsHeaderShown(currentScrollY <= lastScrollY);
+          } else {
+            setIsHeaderShown(true);
+          }
+
+          // 今回のスクロール位置を残す
+          lastScrollY = currentScrollY;
+        });
+      }
+
+      ticking = true;
+    };
+
+    document.removeEventListener('scroll', handleScroll);
+    document.addEventListener('scroll', handleScroll);
+  }, []);
+
+  return isHeaderShown;
+};
