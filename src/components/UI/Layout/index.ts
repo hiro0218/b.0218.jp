adf29582f4d146aa10f3a53d6955f1bccaae0be9
hiro0218 <hiro0218@gmail.com>
2023-01-21 16:44:37 +0900

    fix: Containerを統合

--- src/components/UI/Layout/index.ts
+++ src/components/UI/Layout/index.ts
@@ -1,6 +1,5 @@
 import Columns from './Columns';
-import MainContainer from './Container';
 import PageContentContainer from './PageContentContainer';
 import Stack from './Stack';
 
-export { Stack, Columns, MainContainer, PageContentContainer };
+export { Stack, Columns, PageContentContainer };
