e19c309b2933858650a553d61339df2057d95019
hiro0218 <hiro0218@gmail.com>
2023-01-21 17:01:41 +0900

    fix: as props

--- src/components/UI/Layout/PageContainer.tsx
+++ src/components/UI/Layout/PageContainer.tsx
@@ -1,13 +1,14 @@
-import { memo, ReactNode } from 'react';
+import { ElementType, memo, ReactNode } from 'react';
 
 import { styled } from '@/ui/styled';
 
 type Props = {
+  as?: ElementType;
   children?: ReactNode;
 };
 
-const PageContainer = memo(function PageContainer({ children }: Props) {
-  return <Container>{children}</Container>;
+const PageContainer = memo(function PageContainer({ as, children }: Props) {
+  return <Container as={as}>{children}</Container>;
 });
 
 export default PageContainer;
