917181c773e44b6ebbb7f995352953f75712fb76
hiro0218 <hiro0218@gmail.com>
2023-01-24 00:05:45 +0900

    chore

--- src/components/Page/Home/Hero/index.tsx
+++ src/components/Page/Home/Hero/index.tsx
@@ -13,7 +13,7 @@ export const Hero = memo(function Hero() {
         <img src="/hiro0218@100x100.webp" height={ICON_SIZE} width={ICON_SIZE} alt="" decoding="async" />
       </Avatar>
       <Title
-        heading="Hello, I'm hiro."
+        heading="Hi, I'm hiro."
         paragraph="Web Developer (Frontend) | Development Experience: Backend, iOS App, Windows App"
       />
     </Container>
