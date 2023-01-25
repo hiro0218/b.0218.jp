fb674438197ff889e6c09604fd26ab13e01ba97f
hiro0218 <hiro0218@gmail.com>
2023-01-26 01:07:22 +0900

    fix: 初回表示のチラツキを解消

--- src/components/App/TheHeader/HeaderLayout.tsx
+++ src/components/App/TheHeader/HeaderLayout.tsx
@@ -1,20 +1,25 @@
 import { memo, ReactNode } from 'react';
 
 import { WaveDown } from '@/components/Functional/Wave';
+import useIsMounted from '@/hooks/useIsMounted';
 import { fadeIn, fadeOut } from '@/ui/animation';
 import { css, styled } from '@/ui/styled';
 
 import { useHeaderScrollHandler } from './useHeaderScrollHandler';
+
 type Props = {
   children: ReactNode;
 };
 
 export const HeaderLayout = memo(function HeaderLayout({ children }: Props) {
+  const isMounted = useIsMounted();
   const isHeaderShown = useHeaderScrollHandler();
 
   return (
     <Underline>
-      <Header isShown={isHeaderShown}>{children}</Header>
+      <Header isMounted={isMounted} isShown={isHeaderShown}>
+        {children}
+      </Header>
       <WaveDown fill="var(--component-backgrounds-3)" />
     </Underline>
   );
@@ -24,7 +29,7 @@ const Underline = styled.div`
   height: ${({ theme }) => theme.components.header.height}px;
 `;
 
-const Header = styled.header<{ isShown: boolean }>`
+const Header = styled.header<{ isMounted: boolean; isShown: boolean }>`
   position: fixed;
   z-index: var(--zIndex-header);
   isolation: isolate;
@@ -33,11 +38,14 @@ const Header = styled.header<{ isShown: boolean }>`
   left: 0;
   height: ${({ theme }) => theme.components.header.height}px;
   margin: 0 auto;
-  transition: opacity 0.4s linear;
   pointer-events: none;
   will-change: opacity;
 
-  ${({ isShown }) => {
+  ${({ isMounted, isShown }) => {
+    if (!isMounted) {
+      return;
+    }
+
     return isShown
       ? css`
           animation: ${fadeIn} 0.4s linear both;
--- src/hooks/useIsMounted.ts
+++ src/hooks/useIsMounted.ts
@@ -11,5 +11,5 @@ export default function useIsMounted() {
     };
   }, []);
 
-  return useCallback(() => isMounted.current, []);
+  return useCallback(() => isMounted.current, [])();
 }
