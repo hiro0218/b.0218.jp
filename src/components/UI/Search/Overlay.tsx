8b0c8d481c0c424dbbe1036cc4f2110b2ce46ba3
hiro0218 <hiro0218@gmail.com>
2023-01-25 23:27:03 +0900

    init: Overlay Component

---
+++ src/components/UI/Search/Overlay.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { styled } from '@/ui/styled';
+
+type Props = {
+  isActive: boolean;
+  onClick: () => void;
+};
+
+export const Overlay = ({ isActive, onClick }: Props) => {
+  const [isVisible, setIsVisible] = useState(isActive);
+  const onTransitionEnd = useCallback(() => {
+    if (!isActive) {
+      setIsVisible(false);
+    }
+  }, [isActive]);
+
+  useEffect(() => {
+    setIsVisible(isActive);
+  }, [isActive]);
+
+  return <Div isActive={isActive} isVisible={isVisible} onClick={onClick} onTransitionEnd={onTransitionEnd} />;
+};
+
+const Div = styled.div<{
+  isActive: boolean;
+  isVisible: boolean;
+}>`
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+  position: fixed;
+  transition: opacity 0.4s ease, visibility 0.4s ease;
+  opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+  background-color: var(--overlay-backgrounds);
+  inset: 0;
+  isolation: isolate;
+`;
