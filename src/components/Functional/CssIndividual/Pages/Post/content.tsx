b8070a7e082df18f74be7791c47e1c23db0a4cbd
hiro0218 <hiro0218@gmail.com>
2023-01-29 10:38:56 +0900

    fix: headingの隣接を修正

--- src/components/Functional/CssIndividual/Pages/Post/content.tsx
+++ src/components/Functional/CssIndividual/Pages/Post/content.tsx
@@ -23,17 +23,26 @@ const Headings = css`
     }
   }
 
-  ${[2, 3, 4, 5, 6].map((headingNumber: number) => {
+  ${[2, 3, 4, 5, 6].map((headingNumber) => {
     const tag = `h${headingNumber}`;
+    const nextTagNumber = headingNumber + 1;
+    const nextTagSiblingSelector = nextTagNumber <= 6 ? `${tag} + h${nextTagNumber}` : undefined;
     const style = {
       marginTop: `${headingNumber >= 2 ? 'var(--space-5)' : headingNumber >= 5 ? 'var(--space-4)' : 'var(--space-3)'}`,
       marginBottom: `${headingNumber >= 3 ? 'var(--space-3)' : 'var(--space-2)'}`,
       lineHeight: 1.5,
     };
+    const cancelMarginStyle = {
+      marginTop: 0,
+    };
 
     return css`
       ${tag} {
         ${style}
+      }
+      ${tag} + ${tag},
+      ${nextTagSiblingSelector} {
+        ${cancelMarginStyle}
       }
     `;
   })}
