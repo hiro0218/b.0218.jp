202af3ec1efa4b687d2015273c672426c07e43a4
hiro0218 <hiro0218@gmail.com>
2023-01-30 00:21:36 +0900

    fix: gap

--- src/components/Page/Post/NextRead/index.tsx
+++ src/components/Page/Post/NextRead/index.tsx
@@ -36,11 +36,11 @@ export default PostNextRead;
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(2, minmax(calc(50% - var(--space-3)), max-content));
-  gap: var(--space-3);
+  gap: var(--space-1);
   margin-top: var(--space-3);
 
   ${isMobile} {
     grid-template-columns: minmax(100%, max-content);
-    gap: var(--space-2);
+    gap: var(--space-half);
   }
 `;
