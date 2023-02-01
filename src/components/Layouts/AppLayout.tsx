7440e2a97a120df7ff5e790ddb4b1dba90c8b334
hiro0218 <hiro0218@gmail.com>
2023-02-01 17:38:31 +0900

    fix: dynamic import

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -1,10 +1,12 @@
+import dynamic from 'next/dynamic';
+
 import Footer from '@/components/App/TheFooter';
 import Header from '@/components/App/TheHeader';
-import { PageScroll } from '@/components/UI/PageScroll';
 import { isMobile } from '@/ui/lib/mediaQuery';
 import { styled } from '@/ui/styled';
 
 const CONTAINER_WIDTH = 800;
+const PageScroll = dynamic(() => import('@/components/UI/PageScroll').then((module) => module.PageScroll));
 
 export default function AppLayout({ children }) {
   return (
