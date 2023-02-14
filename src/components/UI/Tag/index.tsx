657a554d70d55340c583b3dcf43f6132c9caa765
hiro0218 <hiro0218@gmail.com>
2023-02-14 14:13:42 +0900

    fix: bg color

--- src/components/UI/Tag/index.tsx
+++ src/components/UI/Tag/index.tsx
@@ -48,7 +48,7 @@ const Anchor = styled(_Anchor)`
   align-self: flex-start;
   padding: 0 var(--space-1);
   border-radius: var(--border-radius-2);
-  background-color: var(--backgrounds-2A);
+  background-color: var(--component-backgrounds-3A);
   color: var(--text-11);
   text-align: center;
   white-space: nowrap;
