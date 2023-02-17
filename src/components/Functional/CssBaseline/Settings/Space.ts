81b48b12dc0714cc81882550e5095c1cdd4e5c01
hiro0218 <hiro0218@gmail.com>
2023-02-17 15:04:46 +0900

    refactor

---
+++ src/components/Functional/CssBaseline/Settings/Space.ts
@@ -0,0 +1,17 @@
+import { css } from '@/ui/styled';
+
+const SPACING_BASE_PX = 8;
+
+export default css`
+  /**
+   * space
+   * 段階はフィボナッチ数列を利用
+   */
+  --space-half: ${SPACING_BASE_PX / 2}px;
+  --space-1: ${SPACING_BASE_PX}px;
+  --space-2: ${SPACING_BASE_PX * 2}px;
+  --space-3: ${SPACING_BASE_PX * 3}px;
+  --space-4: ${SPACING_BASE_PX * 5}px;
+  --space-5: ${SPACING_BASE_PX * 8}px;
+  --space-6: ${SPACING_BASE_PX * 13}px;
+`;
