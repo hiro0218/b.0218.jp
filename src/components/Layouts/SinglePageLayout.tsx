7027d48be0a1a3b46b20f81ab99307649213b588
hiro0218 <hiro0218@gmail.com>
2023-02-27 23:02:05 +0900

    init: SinglePageLayout

---
+++ src/components/Layouts/SinglePageLayout.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import { PropsWithChildren, ReactElement } from 'react';
+
+import { PageContainer } from '@/components/UI/Layout';
+import { Title } from '@/components/UI/Title';
+
+type LayoutProps = {
+  head: {
+    title: string;
+  };
+  content?: string;
+  title: {
+    heading: string;
+    paragraph: string;
+  };
+};
+
+export const createGetLayout = (layoutProps?: LayoutProps): ((children: ReactElement) => ReactElement) => {
+  return function getLayout(children: ReactElement) {
+    return <Layout {...layoutProps}>{children}</Layout>;
+  };
+};
+
+const Layout = ({ children, ...props }: PropsWithChildren<LayoutProps>) => {
+  return (
+    <>
+      <Head>
+        <title key="title">{props.head.title}</title>
+      </Head>
+      <PageContainer as="article">
+        <Title heading={props.title.heading} paragraph={props.title.paragraph} />
+        {children}
+      </PageContainer>
+    </>
+  );
+};
+
+export const getLayout = (page: React.ReactElement) => {
+  return <div data-page="SinglePage">{page}</div>;
+};
