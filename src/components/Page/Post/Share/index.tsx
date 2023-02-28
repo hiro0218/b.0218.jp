507310b634f59bae9aacd9f259b1fbdd4ae907cd
hiro0218 <hiro0218@gmail.com>
2023-02-28 14:55:31 +0900

    add: tooltip

--- src/components/Page/Post/Share/index.tsx
+++ src/components/Page/Post/Share/index.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { ScreenReaderOnlyText } from '@/components/UI/ScreenReaderOnlyText';
 import useCopyToClipboard from '@/hooks/useCopyToClipboard';
+import { fadeIn } from '@/ui/animation';
 import { SiHatenabookmark, SiTwitter } from '@/ui/icons';
 import { RxLink2 } from '@/ui/icons';
 import { showHoverBackground } from '@/ui/mixin';
@@ -28,22 +29,25 @@ const PostShare = ({ title, url }: Props) => {
           href={`https://twitter.com/intent/tweet?url=${url}&text=${encodeURIComponent(title)}`}
           target="_blank"
           rel="noopener noreferrer"
+          title="Twitterでツイートする"
         >
           <IconContainer>
             <SiTwitter size={ICON_SIZE / 2} />
-            <Label>Tweet</Label>
           </IconContainer>
         </Anchor>
-        <Anchor href={`https://b.hatena.ne.jp/entry/panel/?url=${url}`} target="_blank" rel="noopener noreferrer">
+        <Anchor
+          href={`https://b.hatena.ne.jp/entry/panel/?url=${url}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          title="はてなブックマークでブックマークする"
+        >
           <IconContainer>
             <SiHatenabookmark size={ICON_SIZE / 2} />
-            <Label>Bookmark</Label>
           </IconContainer>
         </Anchor>
         <Anchor as="button" type="button" title="記事のURLをコピーする" onClick={onClickCopyPermalink}>
           <IconContainer>
             <RxLink2 size={ICON_SIZE / 2} />
-            <Label>Copy</Label>
           </IconContainer>
         </Anchor>
       </Container>
@@ -59,21 +63,35 @@ const Container = styled.div`
 `;
 
 const Anchor = styled.a`
+  position: relative;
   display: flex;
   justify-content: center;
   line-height: 1;
   text-align: center;
   cursor: pointer;
-`;
 
-const Label = styled.span`
-  position: absolute;
-  top: 100%;
-  display: block;
-  font-size: var(--font-size-sm);
-  line-height: 1.5;
-  color: var(--text-11);
-  white-space: nowrap;
+  &::after {
+    position: absolute;
+    bottom: calc(100% + var(--space-1));
+    left: 50%;
+    z-index: 1;
+    display: none;
+    padding: var(--space-1);
+    font-size: var(--font-size-sm);
+    color: var(--text-11);
+    white-space: nowrap;
+    content: attr(title);
+    background-color: var(--component-backgrounds-4);
+    border-radius: var(--border-radius-4);
+    transform: translateX(-50%);
+  }
+
+  &:hover {
+    &::after {
+      display: block;
+      animation: ${fadeIn} 0.2s linear both;
+    }
+  }
 `;
 
 const IconContainer = styled.span`
