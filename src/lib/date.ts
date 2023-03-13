c3b158450c78435926bebe82597dc368e28e038c
hiro0218 <hiro0218@gmail.com>
2023-03-13 22:59:44 +0900

    replace: isSameDay

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -7,16 +7,6 @@ export const convertDateToSimpleFormat = (dateString: string): string => {
   return dateString.split('T')[0].replace(/-/g, '/');
 };
 
-/**
- * isSameDate
- * @param date
- * @param updated
- */
-export const isSameDate = (date: string, updated: string | null): boolean => {
-  // 日付が入っていない場合は同日
-  if (!updated) {
-    return true;
-  }
-
-  return new Date(date).toDateString() === new Date(updated).toDateString();
+export const isSameDay = (dateA: Date, dateB: Date) => {
+  return dateA.toLocaleDateString() === dateB.toLocaleDateString();
 };
