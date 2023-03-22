82cbd05ab6c08dec4b4d66ecb548c13301c2e9bb
hiro0218 <hiro0218@gmail.com>
2023-03-23 00:45:08 +0900

    fix: childrenとinnerHTMLを受け付けるよう修正

--- src/components/UI/Anchor/index.tsx
+++ src/components/UI/Anchor/index.tsx
@@ -1,10 +1,22 @@
 import Link, { LinkProps } from 'next/link';
 import { ReactNode } from 'react';
 
+type ContentProp =
+  | {
+      children: ReactNode;
+      dangerouslySetInnerHTML?: never;
+    }
+  | {
+      children?: never;
+      dangerouslySetInnerHTML: {
+        __html: string;
+      };
+    };
+
 type Props = {
   title?: string;
-  children: ReactNode;
-} & LinkProps;
+} & LinkProps &
+  ContentProp;
 
 export const Anchor = ({ href, prefetch = false, passHref, title, children, ...rest }: Props) => (
   <Link href={href} {...(!prefetch && { prefetch: false })} passHref={passHref} title={title} {...rest}>
