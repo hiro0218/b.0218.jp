890e24a088f273bc1a3f482ecf459b275f9da2a2
hiro0218 <hiro0218@gmail.com>
2023-03-24 18:00:22 +0900

    chore

--- src/lib/SvgComponentToDataUrlScheme.ts
+++ src/lib/SvgComponentToDataUrlScheme.ts
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 
 export const SvgComponentToDataUrlScheme = (Component: ReactElement) => {
-  const SVG = ReactDOMServer.renderToString(Component);
+  const SVG = renderToString(Component);
   return 'data:image/svg+xml,' + encodeURIComponent(SVG);
 };
