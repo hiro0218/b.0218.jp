04307f55d7c1d3319663c96baff48c2ec73cda62
hiro0218 <hiro0218@gmail.com>
2023-04-02 23:34:48 +0900

    rename

--- src/lib/json-ld.ts
+++ src/lib/json-ld.ts
@@ -1,4 +1,4 @@
-import { BlogPosting, BreadcrumbList, ListItem, WithContext } from 'schema-dts';
+import { BlogPosting, BreadcrumbList, ListItem, Organization, WithContext } from 'schema-dts';
 
 import { AUTHOR, SITE, URL } from '@/constant';
 import { Post } from '@/types/source';
@@ -92,7 +92,7 @@ export const getBreadcrumbStructured = (post: Post) => {
 /**
  * @see https://developers.google.com/search/docs/appearance/structured-data/logo?hl=ja
  */
-export const getLogoStructured = () => {
+export const getOrganizationStructured = (): WithContext<Organization> => {
   return {
     '@context': 'https://schema.org',
     '@type': 'Organization',
