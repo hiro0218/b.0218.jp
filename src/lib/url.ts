7eac5ce4943f18f3a4c4e1c78d6ad43d561b6d5e
hiro0218 <hiro0218@gmail.com>
2023-04-03 10:15:39 +0900

    init: url

---
+++ src/lib/url.ts
@@ -0,0 +1,9 @@
+import { SITE } from '@/constant';
+
+export const getPermalink = (slug: string) => {
+  return `${SITE.URL}${slug}.html`;
+};
+
+export const getOgpImage = (slug: string) => {
+  return `${SITE.URL}images/ogp/${slug}.webp`;
+};
