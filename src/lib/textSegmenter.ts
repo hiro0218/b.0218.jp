99ad9865ee38455d68f1dc838a307ba10982f3b0
hiro0218 <hiro0218@gmail.com>
2023-04-03 14:26:21 +0900

    fix: Segmenterのスコープ変更

--- src/lib/textSegmenter.ts
+++ src/lib/textSegmenter.ts
@@ -1,6 +1,6 @@
-export const textSegmenter = (text: string) => {
-  const segmenterJa = new Intl.Segmenter('ja-JP', { granularity: 'word' });
+const segmenterJa = new Intl.Segmenter('ja-JP', { granularity: 'word' });
 
+export const textSegmenter = (text: string) => {
   return Array.from(segmenterJa.segment(text))
     .map(({ segment }) => segment)
     .join('<wbr>');
