8e5f93fe1d6fa6cb92a591028f5ec969a3f8ecd1
hiro0218 <hiro0218@gmail.com>
2023-04-04 13:19:59 +0900

    add: getHTML

--- src/build/article/dom.ts
+++ src/build/article/dom.ts
@@ -1,5 +1,8 @@
 import { JSDOM, VirtualConsole } from 'jsdom';
 
+const FETCH_HEADERS = { 'User-Agent': 'Twitterbot/1.0' };
+export const FETCH_TIMEOUT = 1000;
+
 /**
  * 不正な CSS を読み込んだ際のパースエラーを握りつぶす
  * @see https://github.com/jsdom/jsdom/issues/2230
@@ -14,4 +17,13 @@ export const getMeta = (html: string) => {
   const meta = document.head.querySelectorAll('meta');
 
   return meta;
+};
+
+export const getHTML = async (url: string) => {
+  return await fetch(url, {
+    headers: FETCH_HEADERS,
+    signal: AbortSignal.timeout(FETCH_TIMEOUT),
+  }).then((res) => {
+    return res.status === 200 ? res.text() : '';
+  });
 };
