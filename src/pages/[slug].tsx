9c52f9af7fee60e6d9d6f984f33f1ceb7e2fae8a
hiro0218 <hiro0218@gmail.com>
2023-05-07 15:22:07 +0900

    replace: serverside function

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -8,23 +8,12 @@ import { PostEdit, PostHeader, PostNote, PostShare, PostSimilar, TagSimilar } fr
 import Mokuji from '@/components/Page/Post/Mokuji';
 import { Adsense } from '@/components/UI/Adsense';
 import { PageContainer } from '@/components/UI/Layout';
-import { Props as PostTagProps } from '@/components/UI/Tag';
-import { AUTHOR_NAME, READ_TIME_SUFFIX, TAG_VIEW_LIMIT } from '@/constant';
+import { AUTHOR_NAME, READ_TIME_SUFFIX } from '@/constant';
 import useTwitterWidgetsLoad from '@/hooks/useTwitterWidgetsLoad';
 import { getBlogPostingStructured, getBreadcrumbStructured, getDescriptionText } from '@/lib/json-ld';
-import { getPostsJson, getSimilarPost, getSimilarTag, getTagsJson } from '@/lib/posts';
-import { textSegmenter } from '@/lib/textSegmenter';
 import { getOgpImage, getPermalink } from '@/lib/url';
-import { Post as PostType, PostSimilar as PostSimilarProps, TermsPostList } from '@/types/source';
+import { getStaticPathsPost, getStaticPropsPost, PostProps } from '@/server/[slug]';
 
-type PostProps = {
-  post: PostType & {
-    tagsWithCount: PostTagProps[];
-    segmentedTitle: string;
-  };
-  similarPost: TermsPostList[];
-  similarTags: PostTagProps[];
-};
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function Post({ post, similarPost, similarTags }: Props) {
@@ -111,82 +100,6 @@ export default function Post({ post, similarPost, similarTags }: Props) {
   );
 }
 
-export const getStaticPaths: GetStaticPaths = () => {
-  const posts = getPostsJson();
-  const paths = posts.map(({ slug }) => ({
-    params: { slug: `${slug}.html` },
-  }));
+export const getStaticPaths: GetStaticPaths = (context) => getStaticPathsPost(context);
 
-  return { paths, fallback: false };
-};
-
-export const getStaticProps: GetStaticProps<PostProps> = (context) => {
-  const posts = getPostsJson();
-  const tagData = getTagsJson();
-  const tagDataWithCount = Object.entries(tagData)
-    .map(([slug, val]) => {
-      return { slug, count: val.length };
-    })
-    .sort((a, b) => b.count - a.count);
-  const slug = (context.params.slug as string).replace('.html', '');
-
-  // slug に一致する post を取得
-  const post = posts.find((post) => post.slug === slug);
-
-  // tagsに件数を追加
-  const tagsWithCount: PostTagProps[] = post.tags.map((slug) => {
-    return tagDataWithCount.find((tag) => tag.slug === slug) || null;
-  });
-
-  // タイトルの文字組み
-  const segmentedTitle = textSegmenter(post.title);
-
-  // 関連記事
-  const getSimilarPostBySlug = (similarPosts: PostSimilarProps, key: string) => {
-    const result = similarPosts.find((slug) => slug.hasOwnProperty(key));
-    return result ? result[key] : null;
-  };
-  const similarPosts = getSimilarPost();
-  const similarPostSlugs = getSimilarPostBySlug(similarPosts, slug);
-  const similarPost = Object.keys(similarPostSlugs).map((slug) => {
-    const { title, date, updated, excerpt } = posts.find((post) => post.slug === slug);
-
-    return {
-      title,
-      slug,
-      date,
-      updated,
-      excerpt,
-    };
-  });
-
-  // 奇数の場合は偶数に寄せる
-  if (similarPost.length % 2 !== 0) {
-    similarPost.pop();
-  }
-
-  // 関連タグ
-  const tag = post.tags[0];
-  const getTagBySlug = (slug: PostTagProps['slug']) => {
-    return Object.entries(tagData).find(([key]) => key === slug);
-  };
-  const similarTagsList = getSimilarTag()[tag];
-  const similarTags: PostTagProps[] = !!similarTagsList
-    ? Object.entries(similarTagsList)
-        .map(([slug]) => {
-          const tag = getTagBySlug(slug);
-          const count = tag.length > 1 ? tag[1].length : 0;
-          return count >= TAG_VIEW_LIMIT ? { slug, count } : null;
-        })
-        .filter((item) => item !== null)
-        .sort((a, b) => b.count - a.count)
-    : [];
-
-  return {
-    props: {
-      post: { ...post, tagsWithCount, segmentedTitle },
-      similarPost,
-      similarTags,
-    },
-  };
-};
+export const getStaticProps: GetStaticProps<PostProps> = (context) => getStaticPropsPost(context);
