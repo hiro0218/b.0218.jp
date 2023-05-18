5e39d76dd2e431158030cd5688a858c530f63047
hiro0218 <hiro0218@gmail.com>
2023-05-19 01:10:13 +0900

    fix: Array.from to spread

--- src/server/[slug]/textSegmenter.ts
+++ src/server/[slug]/textSegmenter.ts
@@ -1,7 +1,7 @@
 const segmenterJa = new Intl.Segmenter('ja-JP', { granularity: 'word' });
 
 export const textSegmenter = (text: string) => {
-  return Array.from(segmenterJa.segment(text))
+  return [...segmenterJa.segment(text)]
     .map(({ segment }) => segment)
     .join('<wbr>');
 };
