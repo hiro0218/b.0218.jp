728640adfd8880a1e2cef506e6e7f498c6364f58
hiro0218 <hiro0218@gmail.com>
2023-05-26 16:29:52 +0900

    chore

--- src/components/UI/Layout/index.ts
+++ src/components/UI/Layout/index.ts
@@ -1,9 +1,3 @@
-
-
-
-
-
-
-export {default as Columns} from './Columns';
-export {default as PageContainer} from './PageContainer';
-export {default as Stack} from './Stack';
+export { default as Columns } from './Columns';
+export { default as PageContainer } from './PageContainer';
+export { default as Stack } from './Stack';
