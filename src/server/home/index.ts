bc2460a217e7a36b2624526fbe1bace263188b9c
hiro0218 <hiro0218@gmail.com>
2023-05-26 11:56:48 +0900

    fix: 関数外へ移動

--- src/server/home/index.ts
+++ src/server/home/index.ts
@@ -3,12 +3,14 @@ type PostListProps = UnpackedArray<ReturnType<typeof getPostsListJson>>;
 
 const POST_DISPLAY_LIMIT = 5;
 
+const posts = getPostsListJson();
+const tagsWithCount = getTagsWithCount();
+
 const pickPosts = ({ title, slug, date, updated, excerpt, tags }: PostListProps): PostListProps => {
   return { title, slug, date, updated, excerpt, tags };
 };
 
 export const getData = () => {
-  const posts = getPostsListJson();
   const recentPosts = posts.slice(0, POST_DISPLAY_LIMIT).map((post) => pickPosts(post));
   const updatesPosts = posts
     .sort((a, b) => {
@@ -22,7 +24,7 @@ export const getData = () => {
     .slice(0, POST_DISPLAY_LIMIT)
     .map((post) => pickPosts(post));
 
-  const tags = getTagsWithCount()
+  const tags = tagsWithCount
     .filter((item, i) => item[1] >= 10 && i < 25) // 件数が10件以上を25個抽出
     .map(([slug, count]) => {
       return { slug, count };
