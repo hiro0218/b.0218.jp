160b5b2c029e4abcded8ae63ef309f955303384b
hiro0218 <hiro0218@gmail.com>
2023-05-30 00:30:19 +0900

    refactor

--- src/build/article/transform/image.ts
+++ src/build/article/transform/image.ts
@@ -1,10 +1,8 @@
 import type { Element } from 'hast';
 
 const transformImage = (node: Element) => {
-  node.properties = {
-    ...(node.properties || {}),
-    loading: 'lazy',
-  };
+  node.properties = node.properties || {};
+  node.properties.loading = 'lazy';
 };
 
 export default transformImage;
