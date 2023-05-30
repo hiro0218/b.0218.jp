0c8df929ff4e2bcaac4d0803736e2560054df49a
hiro0218 <hiro0218@gmail.com>
2023-05-31 00:40:40 +0900

    fix: node prefix

--- src/build/ogp/convertPngToWebP.ts
+++ src/build/ogp/convertPngToWebP.ts
@@ -1,5 +1,6 @@
-import fs from 'fs/promises';
-import path from 'path';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 import sharp from 'sharp';
 
 import * as Log from '@/lib/Log';
