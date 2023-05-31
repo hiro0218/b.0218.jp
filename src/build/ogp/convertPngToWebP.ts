8f008982b5444fc950164329ad28a9c55723f311
hiro0218 <hiro0218@gmail.com>
2023-06-01 00:06:16 +0900

    add: Log

--- src/build/ogp/convertPngToWebP.ts
+++ src/build/ogp/convertPngToWebP.ts
@@ -7,13 +7,17 @@ import * as Log from '@/lib/Log';
 
 const TARGET_DIRECTORY = path.join(process.cwd(), 'public/images/ogp');
 
-async function convertToWebp(file: string) {
+async function convertToWebp(file: string, index: number, total: number) {
   const inputFile = path.join(TARGET_DIRECTORY, file);
   const outputFile = path.join(TARGET_DIRECTORY, `${path.basename(file, '.png')}.webp`);
 
   try {
     await sharp(inputFile).toFormat('webp').toFile(outputFile);
     await fs.unlink(inputFile);
+    const processed = index + 1;
+    if (processed === 1 || processed % 100 === 0 || processed === total) {
+      Log.info('Convert PNG to WebP', `(${processed}/${total})`);
+    }
   } catch (err) {
     Log.error(err.message);
   }
@@ -22,8 +26,9 @@ async function convertToWebp(file: string) {
 async function main() {
   const files = await fs.readdir(TARGET_DIRECTORY);
   const pngFiles = files.filter((file) => path.extname(file) === '.png');
+  const total = pngFiles.length;
 
-  await Promise.all(pngFiles.map(convertToWebp));
+  await Promise.all(pngFiles.map((file, index) => convertToWebp(file, index, total)));
 }
 
 main().catch((err) => Log.error(err.message));
