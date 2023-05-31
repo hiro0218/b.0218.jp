c6e6d505cae973329c88fa063377c073e27f7d3d
hiro0218 <hiro0218@gmail.com>
2023-05-31 21:12:02 +0900

    refactor

--- src/components/Functional/CssIndividual/Pages/Post/Heading.ts
+++ src/components/Functional/CssIndividual/Pages/Post/Heading.ts
@@ -1,5 +1,33 @@
 import { css } from '@/ui/styled';
 
+const generateHeadingStyles = (headingNumber: number) => {
+  const tag = `h${headingNumber}`;
+  const nextTagNumber = headingNumber + 1;
+  const nextTagSiblingSelector = nextTagNumber <= 6 ? `${tag} + h${nextTagNumber}` : undefined;
+  const marginTop = headingNumber >= 2 ? 'var(--space-5)' : headingNumber >= 5 ? 'var(--space-4)' : 'var(--space-3)';
+  const marginBottom = headingNumber >= 3 ? 'var(--space-3)' : 'var(--space-2)';
+
+  const style = {
+    marginTop,
+    marginBottom,
+    lineHeight: 'var(--line-height-md)',
+  };
+
+  const cancelMarginStyle = {
+    marginTop: 0,
+  };
+
+  return css`
+    ${tag} {
+      ${style}
+    }
+    ${tag} + ${tag},
+    ${nextTagSiblingSelector} {
+      ${cancelMarginStyle}
+    }
+  `;
+};
+
 const Headings = css`
   h2,
   h3,
@@ -22,29 +50,7 @@ const Headings = css`
     }
   }
 
-  ${[2, 3, 4, 5, 6].map((headingNumber) => {
-    const tag = `h${headingNumber}`;
-    const nextTagNumber = headingNumber + 1;
-    const nextTagSiblingSelector = nextTagNumber <= 6 ? `${tag} + h${nextTagNumber}` : undefined;
-    const style = {
-      marginTop: `${headingNumber >= 2 ? 'var(--space-5)' : headingNumber >= 5 ? 'var(--space-4)' : 'var(--space-3)'}`,
-      marginBottom: `${headingNumber >= 3 ? 'var(--space-3)' : 'var(--space-2)'}`,
-      lineHeight: 1.5,
-    };
-    const cancelMarginStyle = {
-      marginTop: 0,
-    };
-
-    return css`
-      ${tag} {
-        ${style}
-      }
-      ${tag} + ${tag},
-      ${nextTagSiblingSelector} {
-        ${cancelMarginStyle}
-      }
-    `;
-  })}
+  ${[2, 3, 4, 5, 6].map((number) => generateHeadingStyles(number))}
 `;
 
 export default Headings;
