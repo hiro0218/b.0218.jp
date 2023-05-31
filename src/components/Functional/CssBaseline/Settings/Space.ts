c5dc95b1275f41d90648e1b3d78945af591c3592
hiro0218 <hiro0218@gmail.com>
2023-05-31 19:02:50 +0900

    add: comment

--- src/components/Functional/CssBaseline/Settings/Space.ts
+++ src/components/Functional/CssBaseline/Settings/Space.ts
@@ -7,11 +7,11 @@ export default css`
    * space
    * 段階はフィボナッチ数列を利用
    */
-  --space-½: ${SPACING_BASE_PX / 2}px;
-  --space-1: ${SPACING_BASE_PX}px;
-  --space-2: ${SPACING_BASE_PX * 2}px;
-  --space-3: ${SPACING_BASE_PX * 3}px;
-  --space-4: ${SPACING_BASE_PX * 5}px;
-  --space-5: ${SPACING_BASE_PX * 8}px;
-  --space-6: ${SPACING_BASE_PX * 13}px;
+  --space-½: ${SPACING_BASE_PX / 2}px; // 4
+  --space-1: ${SPACING_BASE_PX}px; // 8
+  --space-2: ${SPACING_BASE_PX * 2}px; // 16
+  --space-3: ${SPACING_BASE_PX * 3}px; // 24
+  --space-4: ${SPACING_BASE_PX * 5}px; // 40
+  --space-5: ${SPACING_BASE_PX * 8}px; // 64
+  --space-6: ${SPACING_BASE_PX * 13}px; // 104
 `;
