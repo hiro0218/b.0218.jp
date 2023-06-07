c8a3508b4bc22ee7a64c857692fd3994f0a0bc37
hiro0218 <hiro0218@gmail.com>
2023-06-07 23:49:25 +0900

    refactor

--- src/client/home/LinkMore/index.tsx
+++ src/client/home/LinkMore/index.tsx
@@ -1,23 +1,15 @@
-import { memo } from 'react';
-
 import { Anchor as _Anchor } from '@/components/UI/Anchor';
 import { LinkStyle } from '@/components/UI/LinkMenu';
 import { ICON_SIZE_XS, RxCaretRight } from '@/ui/icons';
 import { styled } from '@/ui/styled';
 
-const IconArrow = memo(function IconArrow() {
-  return (
-    <Icon>
-      <RxCaretRight size={ICON_SIZE_XS} />
-    </Icon>
-  );
-});
-
 export function LinkMore({ href, text }: { href: string; text: string }) {
   return (
     <Anchor href={href}>
       {text}
-      <IconArrow />
+      <Icon>
+        <RxCaretRight size={ICON_SIZE_XS} />
+      </Icon>
     </Anchor>
   );
 }
--- src/server/home/index.ts
+++ src/server/home/index.ts
@@ -1,34 +1,21 @@
 import { getPostsListJson, getTagsWithCount } from '@/lib/posts';
-type PostListProps = UnpackedArray<ReturnType<typeof getPostsListJson>>;
 
 const POST_DISPLAY_LIMIT = 5;
 
 const posts = getPostsListJson();
 const tagsWithCount = getTagsWithCount();
 
-const pickPosts = ({ title, slug, date, updated, excerpt, tags }: PostListProps): PostListProps => {
-  return { title, slug, date, updated, excerpt, tags };
-};
-
 export const getData = () => {
-  const recentPosts = posts.slice(0, POST_DISPLAY_LIMIT).map((post) => pickPosts(post));
+  const recentPosts = posts.slice(0, POST_DISPLAY_LIMIT);
+  const recentSlugs = new Set(recentPosts.map(({ slug }) => slug));
   const updatesPosts = posts
-    .sort((a, b) => {
-      return a.updated < b.updated ? 1 : -1;
-    })
-    .filter((post) => post.updated && post.date < post.updated)
-    .filter((post) => {
-      // recentPosts に含まれているものは除外する
-      return !recentPosts.filter((recentPost) => post.slug === recentPost.slug).length;
-    })
-    .slice(0, POST_DISPLAY_LIMIT)
-    .map((post) => pickPosts(post));
+    .sort((a, b) => (a.updated < b.updated ? 1 : -1))
+    .filter((post) => post.updated && post.date < post.updated && !recentSlugs.has(post.slug))
+    .slice(0, POST_DISPLAY_LIMIT);
 
   const tags = tagsWithCount
     .filter((item, i) => item[1] >= 10 && i < 25) // 件数が10件以上を25個抽出
-    .map(([slug, count]) => {
-      return { slug, count };
-    });
+    .map(([slug, count]) => ({ slug, count }));
 
   return {
     recentPosts,
