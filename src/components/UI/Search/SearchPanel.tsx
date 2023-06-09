6b8a9ba672f9cfd7e4b3c5e55160a18cb0f24c55
hiro0218 <hiro0218@gmail.com>
2023-06-10 03:00:19 +0900

    refactor

--- src/components/UI/Search/SearchPanel.tsx
+++ src/components/UI/Search/SearchPanel.tsx
@@ -14,11 +14,12 @@ type Props = {
   closeDialog: onCloseDialogProps;
 };
 
+const HIGHLIGHT_TAG_NAME = 'mark';
+
 const markEscapedHTML = (text: string, markTexts: string[]) => {
-  const tagName = 'mark';
   const escapedText = escapeHTML(text);
   const regEx = new RegExp(markTexts.join('|'), 'gi');
-  return escapedText.replace(regEx, `<${tagName}>$&</${tagName}>`);
+  return escapedText.replace(regEx, `<${HIGHLIGHT_TAG_NAME}>$&</${HIGHLIGHT_TAG_NAME}>`);
 };
 
 const Result = memo(function Result({
