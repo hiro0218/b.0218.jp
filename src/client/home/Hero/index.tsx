65808796df35bc03a6a0cf7a343f118d4d2c234f
hiro0218 <hiro0218@gmail.com>
2023-06-15 01:12:46 +0900

    fix

--- src/client/home/Hero/index.tsx
+++ src/client/home/Hero/index.tsx
@@ -5,6 +5,7 @@ import { isMobile } from '@/ui/lib/mediaQuery';
 import { styled } from '@/ui/styled';
 
 const ICON_SIZE = 100;
+const ICON_SIZE_SHRINK = ICON_SIZE * 0.85;
 
 export const Hero = memo(function Hero() {
   return (
@@ -31,15 +32,15 @@ const Avatar = styled.div`
   user-select: none;
 
   ${isMobile} {
-    width: ${ICON_SIZE * 0.85}px;
+    width: ${ICON_SIZE_SHRINK}px;
   }
 
   svg {
     border-radius: var(--border-radius-full);
 
     ${isMobile} {
-      width: ${ICON_SIZE * 0.85}px;
-      height: ${ICON_SIZE * 0.85}px;
+      width: ${ICON_SIZE_SHRINK}px;
+      height: ${ICON_SIZE_SHRINK}px;
     }
   }
 `;
