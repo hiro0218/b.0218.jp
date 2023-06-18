78f5963ab6d26e4702292c607108a8da2fe5928d
hiro0218 <hiro0218@gmail.com>
2023-06-18 23:38:03 +0900

    chore

--- src/components/UI/Tag/index.tsx
+++ src/components/UI/Tag/index.tsx
@@ -28,7 +28,7 @@ const PostTag = memo(function PostTag({ tags }: PostTagProps) {
           return b.count - a.count;
         })
         .map(({ slug, count }) => {
-          const TagComponent = Number(count) >= TAG_VIEW_LIMIT ? Anchor : Anchor.withComponent('span');
+          const TagComponent = count >= TAG_VIEW_LIMIT ? Anchor : Anchor.withComponent('span');
 
           return (
             <TagComponent href={`/tags/${slug}`} key={slug} passHref prefetch={false}>
