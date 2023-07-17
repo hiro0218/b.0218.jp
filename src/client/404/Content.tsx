c9747c35bac19ba50f9e308bbb57aa439089e632
hiro0218 <hiro0218@gmail.com>
2023-07-18 00:12:32 +0900

    fix: style

--- src/client/404/Content.tsx
+++ src/client/404/Content.tsx
@@ -1,23 +1,31 @@
-import { Anchor } from '@/components/UI/Anchor';
+import { Anchor as _Anchor } from '@/components/UI/Anchor';
+import { Stack } from '@/components/UI/Layout';
+import { LinkStyle } from '@/components/UI/LinkMenu';
+import { Title } from '@/components/UI/Title';
 import { styled } from '@/ui/styled';
 
 export default function Content() {
   return (
-    <Container>
-      <Paragraph>ページが見つかりませんでした。</Paragraph>
-      <Anchor href="/">Home</Anchor>
+    <Container space="3">
+      <Title heading="Page not found" paragraph="ページが見つかりませんでした" />
+      <p>アクセスしようとしたページは、削除もしくはURLが変更され利用できない可能性があります。</p>
+      <Anchor href="/">Home へ戻る</Anchor>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled(Stack)`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 200px;
-  color: var(--text-11);
+  height: 100%;
+  text-align: center;
 `;
 
-const Paragraph = styled.p`
-  margin-right: var(--space-1);
+const Anchor = styled(_Anchor)`
+  ${LinkStyle}
+
+  padding: var(--space-1) var(--space-2);
+  font-size: var(--font-size-md);
 `;
