038145e8a77cbfebd2868320a7308fc10fee5fbe
hiro0218 <hiro0218@gmail.com>
2023-07-25 00:12:27 +0900

    fix

--- src/server/post/getStaticPathsPost.ts
+++ src/server/post/getStaticPathsPost.ts
@@ -1,9 +1,8 @@
-import type { GetStaticPaths, GetStaticPathsContext } from 'next';
+import type { GetStaticPaths } from 'next';
 
 import { getPostsJson } from '@/lib/posts';
 
-// eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
-export const getStaticPathsPost: GetStaticPaths = (context: GetStaticPathsContext) => {
+export const getStaticPathsPost: GetStaticPaths = () => {
   const posts = getPostsJson();
   const paths = [];
 
