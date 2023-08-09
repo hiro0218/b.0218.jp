fb0752e57ccd0caf12f86dfdfa6931dc781ececc
hiro0218 <hiro0218@gmail.com>
2023-08-10 02:16:53 +0900

    update: 分岐を追加

--- src/client/home/PostSection/index.tsx
+++ src/client/home/PostSection/index.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps } from 'react';
+import { type ComponentProps, useMemo } from 'react';
 
 import { Columns, Stack } from '@/components/UI/Layout';
 import LinkCard from '@/components/UI/LinkCard';
@@ -8,16 +8,29 @@ type Props = {
   posts: PostListProps[];
   title: ComponentProps<typeof Columns>['title'];
   titleTagName: ComponentProps<typeof Columns>['titleTagName'];
+  isColumn?: boolean;
 };
 
-export const PostSection = ({ posts, title, titleTagName = 'h3' }: Props) => {
-  return (
-    <Columns title={title} titleTagName={titleTagName}>
+export const PostSection = ({ posts, title, titleTagName = 'h3', isColumn = true }: Props) => {
+  const PostsListComponent = useMemo(() => {
+    return (
       <Stack space="½">
         {posts.map(({ date, slug, tags, title, updated }) => (
           <LinkCard date={date} key={slug} link={`${slug}.html`} tags={tags} title={title} updated={updated} />
         ))}
       </Stack>
-    </Columns>
+    );
+  }, [posts]);
+
+  return (
+    <>
+      {isColumn ? (
+        <Columns title={title} titleTagName={titleTagName}>
+          {PostsListComponent}
+        </Columns>
+      ) : (
+        <>{PostsListComponent}</>
+      )}
+    </>
   );
 };
