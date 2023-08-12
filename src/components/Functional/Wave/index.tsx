69859cc77aa6a5a4c789a385c5f6852278fe5046
hiro0218 <hiro0218@gmail.com>
2023-08-12 17:50:38 +0900

    fix: style position

--- src/components/Functional/Wave/index.tsx
+++ src/components/Functional/Wave/index.tsx
@@ -24,6 +24,9 @@ export const WaveBottom = memo(function WaveBottom() {
 });
 
 const Container = styled.div<Props>`
+  position: relative;
+  top: 2px;
+  z-index: -1;
   display: flex;
   overflow: hidden;
   pointer-events: none;
