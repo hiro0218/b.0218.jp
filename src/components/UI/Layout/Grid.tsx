69f1566f1bd9a1ef563227ebeece681787565eff
hiro0218 <hiro0218@gmail.com>
2023-08-20 23:26:57 +0900

    init: Grid

---
+++ src/components/UI/Layout/Grid.tsx
@@ -0,0 +1,33 @@
+import type { AriaRole, ReactNode } from 'react';
+import { memo, useMemo } from 'react';
+
+import { isMobile } from '@/ui/lib/mediaQuery';
+import { styled } from '@/ui/styled';
+
+type Props = {
+  as?: keyof JSX.IntrinsicElements;
+  gap?: '½' | '1' | '2' | '3' | '4' | '5' | '6';
+  role?: AriaRole;
+  children: ReactNode;
+};
+
+export const Grid = memo(function Grid({ as = 'div', role, children, ...props }: Props) {
+  const MemoizedGridRoot = useMemo(() => GridRoot.withComponent(as), [as]);
+
+  return (
+    <MemoizedGridRoot as={as} role={role} {...props}>
+      {children}
+    </MemoizedGridRoot>
+  );
+});
+
+const GridRoot = styled.div<Props>`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(50% - var(--space-1)), max-content));
+  gap: var(--space-1);
+
+  ${isMobile} {
+    grid-template-columns: minmax(100%, max-content);
+    gap: var(--space-½);
+  }
+`;
--- src/components/UI/Layout/index.ts
+++ src/components/UI/Layout/index.ts
@@ -1,3 +1,4 @@
 export { Columns } from './Columns';
+export { Grid } from './Grid';
 export { SimpleGrid } from './SimpleGrid';
 export { Stack } from './Stack';
