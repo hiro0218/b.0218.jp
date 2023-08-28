3aaa97141b4ca6727decb79a78f2c8c3be9fce19
hiro0218 <hiro0218@gmail.com>
2023-08-28 16:16:28 +0900

    refactor

--- src/lib/escapeHTML.ts
+++ src/lib/escapeHTML.ts
@@ -1,3 +1,13 @@
 export default function escapeHTML(html: string) {
-  return html.replaceAll('&', '&amp;').replaceAll('"', '&quot;').replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+  if (typeof html !== 'string') {
+    return html;
+  }
+
+  return html
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#x27;')
+    .replaceAll('`', '&#x60;');
 }
