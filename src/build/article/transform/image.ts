c6bb06f9e9c5d3a6dcf6f577b48a9a68c34cef7f
hiro0218 <hiro0218@gmail.com>
2023-08-29 02:12:23 +0900

    add: decoding=async

--- src/build/article/transform/image.ts
+++ src/build/article/transform/image.ts
@@ -4,6 +4,7 @@ const transformImage = (node: Element, counter: number) => {
   node.properties = node.properties || {};
   if (counter > 0) {
     node.properties.loading = 'lazy';
+    node.properties.decoding = 'async';
   }
 };
 
