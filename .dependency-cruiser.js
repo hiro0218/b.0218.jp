9336ca74e89028782eccff74ac2217a9b908b00b
hiro0218 <hiro0218@gmail.com>
2023-09-17 16:41:16 +0900

    update: translate

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -6,8 +6,7 @@ module.exports = {
       name: 'no-circular',
       severity: 'warn',
       comment:
-        'This dependency is part of a circular relationship. You might want to revise ' +
-        'your solution (i.e. use dependency inversion, make sure the modules have a single responsibility) ',
+        'この依存関係は循環関係になっています。解決策を再評価することが推奨されます（例えば、依存関係の反転を使用する、モジュールが一つの責任だけを持つようにする等）。',
       from: {},
       to: {
         circular: true,
@@ -16,11 +15,7 @@ module.exports = {
     {
       name: 'no-orphans',
       comment:
-        "This is an orphan module - it's likely not used (anymore?). Either use it or " +
-        "remove it. If it's logical this module is an orphan (i.e. it's a config file), " +
-        'add an exception for it in your dependency-cruiser configuration. By default ' +
-        'this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration ' +
-        'files (.d.ts), tsconfig.json and some of the babel and webpack configs.',
+        'このモジュールは孤立しており、恐らくもはや使用されていない可能性があります。使用するか削除するか、あるいは依存関係の設定でこのモジュールに対する例外を追加してください。このルールはデフォルトでは、いくつかの特定のファイル（例：.eslintrc.jsやtsconfig.json）には適用されません。',
       severity: 'warn',
       from: {
         orphan: true,
@@ -36,8 +31,7 @@ module.exports = {
     {
       name: 'no-deprecated-core',
       comment:
-        'A module depends on a node core module that has been deprecated. Find an alternative - these are ' +
-        "bound to exist - node doesn't deprecate lightly.",
+        'このモジュールは非推奨のNodeのコアモジュールに依存しています。代替案が存在するはずですので、それを探してください。Nodeは軽々しく非推奨にしないので、注意が必要です。',
       severity: 'warn',
       from: {},
       to: {
@@ -69,8 +63,7 @@ module.exports = {
     {
       name: 'not-to-deprecated',
       comment:
-        'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
-        'version of that module, or find an alternative. Deprecated modules are a security risk.',
+        'このモジュールは非推奨のnpmモジュール（またはそのバージョン）に依存しています。遅いバージョンにアップグレードするか、代わりとなるモジュールを見つけてください。非推奨のモジュールはセキュリティリスクとなる可能性があります。',
       severity: 'warn',
       from: {},
       to: {
@@ -81,10 +74,7 @@ module.exports = {
       name: 'no-non-package-json',
       severity: 'error',
       comment:
-        "This module depends on an npm package that isn't in the 'dependencies' section of your package.json. " +
-        "That's problematic as the package either (1) won't be available on live (2 - worse) will be " +
-        'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies ' +
-        'in your package.json.',
+        'このモジュールは、package.jsonのdependenciesセクションに記載されていないnpmパッケージに依存しています。この状態は問題であり、（1）本番環境で利用できないか、（2）本番環境で保証されていないバージョンで利用される可能性があります。package.jsonのdependenciesにこのパッケージを追加してください。',
       from: {},
       to: {
         dependencyTypes: ['npm-no-pkg', 'npm-unknown'],
@@ -93,8 +83,7 @@ module.exports = {
     {
       name: 'not-to-unresolvable',
       comment:
-        "This module depends on a module that cannot be found ('resolved to disk'). If it's an npm " +
-        'module: add it to your package.json. In all other cases you likely already know what to do.',
+        'このモジュールはディスク上で解決できないモジュールに依存しています。npmモジュールであればpackage.jsonに追加する必要があります。',
       severity: 'error',
       from: {},
       to: {
@@ -104,9 +93,7 @@ module.exports = {
     {
       name: 'no-duplicate-dep-types',
       comment:
-        "Likely this module depends on an external ('npm') package that occurs more than once " +
-        'in your package.json i.e. bot as a devDependencies and in dependencies. This will cause ' +
-        'maintenance problems later on.',
+        'このモジュールは、package.json内で複数回（例えば、devDependenciesとdependenciesの両方で）記載されているnpmパッケージに依存しています。これは後にメンテナンスの問題を引き起こす可能性があります。',
       severity: 'warn',
       from: {},
       to: {
@@ -122,9 +109,7 @@ module.exports = {
     {
       name: 'not-to-spec',
       comment:
-        'This module depends on a spec (test) file. The sole responsibility of a spec file is to test code. ' +
-        "If there's something in a spec that's of use to other modules, it doesn't have that single " +
-        'responsibility anymore. Factor it out into (e.g.) a separate utility/ helper or a mock.',
+        'このモジュールはテストファイル（spec）に依存しています。テストファイルはコードをテストする単一の責任を持つべきです。もしその外の何かに使用されている場合は、それを（例えば）別のユーティリティやヘルパーに分離してください。',
       severity: 'error',
       from: {},
       to: {
@@ -135,11 +120,7 @@ module.exports = {
       name: 'not-to-dev-dep',
       severity: 'error',
       comment:
-        "This module depends on an npm package from the 'devDependencies' section of your " +
-        'package.json. It looks like something that ships to production, though. To prevent problems ' +
-        "with npm packages that aren't there on production declare it (only!) in the 'dependencies'" +
-        'section of your package.json. If this module is development only - add it to the ' +
-        'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
+        'このモジュールはpackage.jsonのdevDependenciesセクションにリストされているnpmパッケージに依存していますが、本番環境にデプロイされる可能性があります。package.jsonのdependenciesセクションにのみ記載するようにしてください。',
       from: {
         path: '^(src)',
         pathNot: '\\.(spec|test)\\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\\.md)$',
@@ -152,10 +133,7 @@ module.exports = {
       name: 'optional-deps-used',
       severity: 'info',
       comment:
-        'This module depends on an npm package that is declared as an optional dependency ' +
-        "in your package.json. As this makes sense in limited situations only, it's flagged here. " +
-        "If you're using an optional dependency here by design - add an exception to your" +
-        'dependency-cruiser configuration.',
+        'このモジュールはpackage.jsonでオプションとして指定されているnpmパッケージに依存しています。これは限られた状況でしか意味をなさないため、注意が必要です。',
       from: {},
       to: {
         dependencyTypes: ['npm-optional'],
@@ -164,10 +142,7 @@ module.exports = {
     {
       name: 'peer-deps-used',
       comment:
-        'This module depends on an npm package that is declared as a peer dependency ' +
-        'in your package.json. This makes sense if your package is e.g. a plugin, but in ' +
-        'other cases - maybe not so much. If the use of a peer dependency is intentional ' +
-        'add an exception to your dependency-cruiser configuration.',
+        'このモジュールはpackage.jsonでピア依存として指定されているnpmパッケージに依存しています。このモジュールがプラグイン等であれば問題ないですが、それ以外の場合は注意が必要です。',
       severity: 'warn',
       from: {},
       to: {
