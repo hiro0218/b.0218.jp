764591296340ab94696ca39b6635bc5288754b7c
hiro0218 <hiro0218@gmail.com>
2023-09-22 15:44:51 +0900

    fix: ignore tag

--- src/server/_common/getPopularPost.ts
+++ src/server/_common/getPopularPost.ts
@@ -7,8 +7,9 @@ const popularPostsSlugs = getPostsPopular();
 export const getPopularPost = (posts: PostListProps[], displayLimit: number) => {
   const popularPosts = Object.keys(popularPostsSlugs)
     .filter((slug) => !IGNORE_SLUGS.includes(slug))
-    .slice(0, displayLimit)
-    .map((slug) => posts.find((post) => post.slug === slug));
+    .map((slug) => posts.find((post) => post.slug === slug))
+    .filter(Boolean)
+    .slice(0, displayLimit);
 
   return popularPosts;
 };
--- src/server/home/index.ts
+++ src/server/home/index.ts
@@ -3,20 +3,23 @@ import { getPostsListJson, getTagsWithCount } from '@/lib/posts';
 import { getPopularPost } from '../_common/getPopularPost';
 
 const POST_DISPLAY_LIMIT = 4;
+const IGNORE_TAG = '名探偵コナン';
 
 const posts = getPostsListJson();
 const tagsWithCount = getTagsWithCount();
 
 export const getData = () => {
-  const popularPosts = getPopularPost(posts, POST_DISPLAY_LIMIT);
-  const recentPosts = posts.slice(0, POST_DISPLAY_LIMIT);
+  // 特定のタグを除外
+  const filteredPosts = posts.filter((post) => !post.tags.includes(IGNORE_TAG));
+  const popularPosts = getPopularPost(filteredPosts, POST_DISPLAY_LIMIT);
+  const recentPosts = filteredPosts.slice(0, POST_DISPLAY_LIMIT);
   const recentSlugs = new Set(recentPosts.map(({ slug }) => slug));
-  const updatesPosts = posts
+  const updatesPosts = filteredPosts
     .sort((a, b) => (a.updated < b.updated ? 1 : -1))
-    .filter((post) => post.updated && post.date < post.updated && !recentSlugs.has(post.slug))
+    .filter((post) => !recentSlugs.has(post.slug) && post.updated && post.date < post.updated)
     .slice(0, POST_DISPLAY_LIMIT);
 
-  const tags = tagsWithCount.filter((item, i) => item.count >= 10 && i < 25); // 件数が10件以上を25個抽出
+  const tags = tagsWithCount.filter((item, i) => !item.slug.includes(IGNORE_TAG) && item.count >= 10 && i < 25); // 件数が10件以上を25個抽出
 
   return {
     recentPosts,
