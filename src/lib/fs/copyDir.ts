8b65d7b3320321438f2957e7fc8a76fa02aa6442
hiro0218 <hiro0218@gmail.com>
2023-09-23 20:21:40 +0900

    fix

--- src/lib/fs/copyDir.ts
+++ src/lib/fs/copyDir.ts
@@ -7,51 +7,52 @@ import * as Log from '@/lib/Log';
 export const copyDir = async (srcDir: string, distDir: string) => {
   let results: string[] = [];
   let list: string[] = [];
+
   try {
-    try {
-      list = await readdir(srcDir);
-    } catch (e) {
-      Log.error(`Failed to read directory ${srcDir}: ${e.message}`);
-      return results;
-    }
-
-    for (const file of list) {
-      const src = path.join(srcDir, file);
-      const dst = path.join(distDir, file);
-      let fileStatus: Awaited<ReturnType<typeof stat>>;
-
-      try {
-        fileStatus = await stat(src);
-      } catch (e) {
-        Log.error(`Failed to get file status for ${src}: ${e.message}`);
-        continue;
-      }
-
-      if (fileStatus.isDirectory()) {
-        try {
-          await mkdir(dst);
-        } catch (e) {
-          if (e.code !== 'EEXIST') {
-            Log.error(`Failed to create directory ${dst}: ${e.message}`);
-          } else {
-            Log.info(`Directory already exists: ${dst}`);
-          }
-        }
-        results = results.concat(await copyDir(src, dst));
-      } else {
-        try {
-          Log.info(`Copying file: ${dst}`);
-          const readStream = createReadStream(src);
-          const writeStream = createWriteStream(dst);
-          readStream.pipe(writeStream);
-        } catch (e) {
-          Log.error(`Failed to copy file ${dst}: ${e.message}`);
-        }
-        results.push(src);
-      }
-    }
+    list = await readdir(srcDir);
   } catch (e) {
-    Log.error('copyDir', e);
+    Log.error(`Failed to read directory ${srcDir}: ${e.message}`);
+    return results;
+  }
+
+  for (const file of list) {
+    const src = path.join(srcDir, file);
+    const dst = path.join(distDir, file);
+    let fileStatus: Awaited<ReturnType<typeof stat>>;
+
+    try {
+      fileStatus = await stat(src);
+    } catch (e) {
+      Log.error(`Failed to get file status for ${src}: ${e.message}`);
+      continue;
+    }
+
+    if (fileStatus.isDirectory()) {
+      try {
+        await mkdir(dst, { recursive: true });
+      } catch (e) {
+        if (e.code !== 'EEXIST') {
+          Log.error(`Failed to create directory ${dst}: ${e.message}`);
+        } else {
+          Log.info(`Directory already exists: ${dst}`);
+        }
+      }
+      results = results.concat(await copyDir(src, dst));
+    } else {
+      try {
+        Log.info(`Copying file: ${dst}`);
+        const readStream = createReadStream(src);
+        const writeStream = createWriteStream(dst);
+        readStream.pipe(writeStream);
+
+        // エラーハンドリングの追加
+        readStream.on('error', (err) => Log.error(`Read stream error: ${err.message}`));
+        writeStream.on('error', (err) => Log.error(`Write stream error: ${err.message}`));
+      } catch (e) {
+        Log.error(`Failed to copy file ${dst}: ${e.message}`);
+      }
+      results.push(src);
+    }
   }
 
   return results;
