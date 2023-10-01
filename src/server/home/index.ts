cb7a25293c03a9ba8fb65150a80e1e3264ff2e16
hiro0218 <hiro0218@gmail.com>
2023-10-02 00:27:11 +0900

    refactor

--- src/server/home/index.ts
+++ src/server/home/index.ts
@@ -1,4 +1,3 @@
-import { compareIsoStrings } from '@/lib/compareIsoStrings';
 import { getPostsListJson, getTagsWithCount } from '@/lib/posts';
 
 import { getPopularPost } from '../_common/getPopularPost';
@@ -12,14 +11,16 @@ const tagsWithCount = getTagsWithCount();
 export const getData = () => {
   // 特定のタグを除外
   const filteredPosts = posts.filter((post) => !post.tags.includes(IGNORE_TAG));
-  const popularPosts = getPopularPost(filteredPosts, POST_DISPLAY_LIMIT);
+
   const recentPosts = filteredPosts.slice(0, POST_DISPLAY_LIMIT);
   const recentSlugs = new Set(recentPosts.map(({ slug }) => slug));
   const updatesPosts = filteredPosts
-    .sort((a, b) => compareIsoStrings(b.updated, a.updated))
-    .filter((post) => !recentSlugs.has(post.slug) && post.updated && post.date < post.updated)
+    .filter((post) => !!post.updated && post.date < post.updated)
+    .sort((a, b) => b.updated.localeCompare(a.updated))
+    .filter((post) => !recentSlugs.has(post.slug))
     .slice(0, POST_DISPLAY_LIMIT);
 
+  const popularPosts = getPopularPost(filteredPosts, POST_DISPLAY_LIMIT);
   const tags = tagsWithCount.filter((item, i) => !item.slug.includes(IGNORE_TAG) && item.count >= 10 && i < 25); // 件数が10件以上を25個抽出
 
   return {
