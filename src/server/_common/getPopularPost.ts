c40f5ee6bf6ee25fbf5a67cb4cdec51efca2bdfe
hiro0218 <hiro0218@gmail.com>
2023-12-13 01:50:12 +0900

    fix

--- src/server/_common/getPopularPost.ts
+++ src/server/_common/getPopularPost.ts
@@ -7,7 +7,12 @@ const popularPostsSlugs = getPostsPopular();
 export const getPopularPost = (posts: PostListProps[], displayLimit: number) => {
   const postsMap = new Map(posts.map((post) => [post.slug, post]));
 
-  const popularPosts = Object.keys(popularPostsSlugs)
+  // popularPostsSlugsを配列に変換し、数値が多い順にソート
+  const sortedSlugs = Object.entries(popularPostsSlugs)
+    .sort((a, b) => b[1] - a[1])
+    .map(([slug]) => slug);
+
+  const popularPosts = sortedSlugs
     .filter((slug) => !IGNORE_SLUGS.has(slug))
     .map((slug) => postsMap.get(slug))
     .filter((post) => post !== undefined)
