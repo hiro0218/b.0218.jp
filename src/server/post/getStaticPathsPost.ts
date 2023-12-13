9a4e53c6ff41dc9f09934ee314291fe14dcc51a4
hiro0218 <hiro0218@gmail.com>
2023-12-13 21:20:33 +0900

    fix: loop

--- src/server/post/getStaticPathsPost.ts
+++ src/server/post/getStaticPathsPost.ts
@@ -6,11 +6,11 @@ export const getStaticPathsPost: GetStaticPaths = () => {
   const posts = getPostsJson();
   const paths = [];
 
-  posts.forEach((post) => {
+  for (const post of posts.values()) {
     paths.push({
       params: { post: `${post.slug}.html` },
     });
-  });
+  }
 
   return { paths, fallback: false };
 };
