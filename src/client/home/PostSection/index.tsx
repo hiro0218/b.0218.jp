652b2521bb7953c1aae71ad0d956ede63260840e
hiro0218 <hiro0218@gmail.com>
2023-12-17 21:04:40 +0900

    fix: grid

--- src/client/home/PostSection/index.tsx
+++ src/client/home/PostSection/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const PostSection = ({ posts }: Props) => {
   return (
-    <Grid>
+    <Grid gap="1">
       {posts.map(({ date, slug, tags, title, updated }) => (
         <LinkCard
           date={date}
