bb68d37a7f2d9bc702167a0a2dca7eed12d84322
hiro <hiro0218@gmail.com>
2024-03-01 01:05:15 +0900

    replace: cssnano -> postcss-lightningcss

--- src/lib/processedCss.ts
+++ src/lib/processedCss.ts
@@ -1,21 +1,25 @@
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import postcss from 'postcss';
 import combineSelectors from 'postcss-combine-duplicated-selectors';
+import postcssLightningcss from 'postcss-lightningcss';
 import postcssSortMediaQueries from 'postcss-sort-media-queries';
 
 import packageJson from '@/../package.json';
 
 export const processedCss = (css: string) => {
-  return postcss([
-    autoprefixer({
-      overrideBrowserslist: packageJson.browserslist,
+  const result = postcss([
+    postcssLightningcss({
+      browsers: packageJson.browserslist,
+      lightningcssOptions: {
+        minify: true,
+        sourceMap: false,
+        drafts: {
+          nesting: false,
+        },
+      },
     }),
-    cssnano({
-      preset: ['cssnano-preset-advanced'],
-      plugins: [],
-    }),
-    postcssSortMediaQueries,
+    postcssSortMediaQueries(),
     combineSelectors({ removeDuplicatedProperties: true }),
   ]).process(css).css;
+
+  return result;
 };
