177f3db5c4789f4e7991f8c0b87d833b6de3a5d3
hiro <hiro0218@gmail.com>
2024-04-05 17:41:28 +0900

    remove: useId

--- src/pages/[post]/_components/Similar/Tag.tsx
+++ src/pages/[post]/_components/Similar/Tag.tsx
@@ -1,5 +1,3 @@
-import { useId } from 'react';
-
 import Heading from '@/components/UI/Heading';
 import { SimpleGrid, Stack } from '@/components/UI/Layout';
 import type { Props as PostTagProps } from '@/components/UI/Tag';
@@ -10,15 +8,13 @@ type Props = {
 };
 
 function TagSimilar({ tags }: Props) {
-  const labelledbyId = useId();
-
   if (!tags.length) {
     return null;
   }
 
   return (
-    <Stack aria-labelledby={labelledbyId} as="aside" space="2">
-      <Heading as="h2" id={labelledbyId} text="関連タグ" />
+    <Stack as="aside" space="2">
+      <Heading as="h2" text="関連タグ" />
       <Stack>
         <SimpleGrid isWide={false}>
           <PostTag tags={tags} />
