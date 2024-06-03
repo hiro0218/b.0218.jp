6ebfec87f731b99739f85b4f19dc03b9f23d930d
hiro <hiro0218@gmail.com>
2024-06-04 02:45:45 +0900

    update: cache structure

--- src/pages/_libs/getRecentAndUpdatedPosts.ts
+++ src/pages/_libs/getRecentAndUpdatedPosts.ts
@@ -3,16 +3,25 @@ import type { PostListProps } from '@/types/source';
 import { POST_DISPLAY_LIMIT } from './constant';
 
 type Props = {
-  filteredPosts: PostListProps[];
+  posts: PostListProps[];
 };
 
-export const getRecentAndUpdatedPosts = ({ filteredPosts }: Props) => {
-  const recentPosts = filteredPosts.slice(0, POST_DISPLAY_LIMIT);
+const cache = new Map<string, { recentPosts: PostListProps[]; updatesPosts: PostListProps[] }>();
+const CACHE_KEY = 'RECENT_AND_UPDATED_POSTS_CACHE';
+
+export const getRecentAndUpdatedPosts = ({ posts }: Props) => {
+  if (cache.has(CACHE_KEY)) {
+    return cache.get(CACHE_KEY)!;
+  }
+
+  const recentPosts = posts.slice(0, POST_DISPLAY_LIMIT);
   const recentSlugs = new Set(recentPosts.map(({ slug }) => slug));
-  const updatesPosts = filteredPosts
+  const updatesPosts = posts
     .filter((post) => !!post.updated && post.date < post.updated && !recentSlugs.has(post.slug))
     .sort((a, b) => b.updated.localeCompare(a.updated))
     .slice(0, POST_DISPLAY_LIMIT);
+
+  cache.set(CACHE_KEY, { recentPosts, updatesPosts });
 
   return {
     recentPosts,
--- src/pages/_libs/index.ts
+++ src/pages/_libs/index.ts
@@ -9,7 +9,7 @@ const tagsWithCount = getTagsWithCount();
 
 export const getData = () => {
   const filteredPosts = getFilteredPosts();
-  const { recentPosts, updatesPosts } = getRecentAndUpdatedPosts({ filteredPosts });
+  const { recentPosts, updatesPosts } = getRecentAndUpdatedPosts({ posts: filteredPosts });
   const popularPosts = getPopularPost(filteredPosts, POST_DISPLAY_LIMIT);
 
   const tags = tagsWithCount.filter(({ slug, count }) => !IGNORE_TAGS.has(slug) && count >= 10).slice(0, 25);
