b4c7e7aafdcee6be1bd2b2b631f970e98b54b904
hiro <hiro0218@gmail.com>
2024-06-17 02:31:44 +0900

    remove: Icon sIze

--- src/ui/styled/iconSets.tsx
+++ src/ui/styled/iconSets.tsx
@@ -1,8 +1,8 @@
 import { SvgComponentToDataUrlScheme } from '@/lib/SvgComponentToDataUrlScheme';
-import { ExternalLinkIcon, ICON_SIZE_XS } from '@/ui/icons';
+import { ExternalLinkIcon } from '@/ui/icons';
 import { css } from '@/ui/styled';
 
-const IconExternalLink = SvgComponentToDataUrlScheme(<ExternalLinkIcon height={ICON_SIZE_XS} width={ICON_SIZE_XS} />);
+const IconExternalLink = SvgComponentToDataUrlScheme(<ExternalLinkIcon />);
 
 /** 疑似要素に指定する */
 export const IconExternalLinkStyle = css`
