87460472a5b61ea047b5ba0358afa87d6f013b78
hiro <hiro0218@gmail.com>
2024-08-25 20:16:06 +0900

    update: 404 style

--- src/pages/_components/404/Content.tsx
+++ src/pages/_components/404/Content.tsx
@@ -1,27 +1,48 @@
 import { Anchor as _Anchor } from '@/components/UI/Anchor';
 import { Stack } from '@/components/UI/Layout';
-import { LinkStyle } from '@/components/UI/LinkMenu';
-import { Title } from '@/components/UI/Title';
 import { styled } from '@/ui/styled';
 
 export default function Content() {
   return (
     <Container align="center" direction="vertical" justify="center" space={3}>
-      <Title heading="Page not found" paragraph="ページが見つかりませんでした" />
-      <p>アクセスしようとしたページは、削除もしくはURLが変更され利用できない可能性があります。</p>
-      <Anchor href="/">Home へ戻る</Anchor>
+      <Header>
+        <h1>Page not found</h1>
+        <p>ページが見つかりませんでした</p>
+      </Header>
+      <Stack space={4}>
+        <p>アクセスしようとしたページは、削除もしくはURLが変更され利用できない可能性があります。</p>
+        <Anchor href="/">トップへ戻る</Anchor>
+      </Stack>
     </Container>
   );
 }
 
 const Container = styled(Stack)`
   height: 100%;
+  padding-inline: var(--space-4);
   text-align: center;
 `;
 
-const Anchor = styled(_Anchor)`
-  ${LinkStyle}
+const Header = styled.header`
+  h1 {
+    font-size: var(--font-size-h1);
+  }
 
+  p {
+    font-size: var(--font-size-sm);
+    color: var(--text-11);
+  }
+`;
+
+const Anchor = styled(_Anchor)`
+  width: 120px;
   padding: var(--space-1) var(--space-2);
-  font-size: var(--font-size-md);
+  margin-inline: auto;
+  font-size: var(--font-size-sm);
+  background-color: var(--component-backgrounds-3);
+  border-radius: var(--border-radius-4);
+
+  &:hover {
+    background-color: var(--component-backgrounds-4);
+  }
 `;
