0bd5dd637c018541a7845b17f97aa63eb47fd153
hiro <hiro0218@gmail.com>
2024-10-25 22:52:45 +0900

    fix: データをsortした状態にする

--- src/lib/fs/writeJSON.ts
+++ src/lib/fs/writeJSON.ts
@@ -1,8 +1,6 @@
 import { writeFile } from 'node:fs/promises';
 
-import stringify from 'fast-json-stable-stringify';
-
 export const writeJSON = async (path: string, json: unknown) => {
-  const data = stringify(json);
+  const data = JSON.stringify(json);
   await writeFile(path, data);
 };
--- src/pages/_libs/getPopularPost.ts
+++ src/pages/_libs/getPopularPost.ts
@@ -6,9 +6,7 @@ const popularPostsSlugs = getPostsPopular();
 
 export const getPopularPost = (posts: ReturnType<typeof getPostsListJson>, displayLimit: number) => {
   // popularPostsSlugsを配列に変換し、数値が多い順にソート
-  const sortedSlugs = Object.entries(popularPostsSlugs)
-    .sort((a, b) => b[1] - a[1])
-    .map(([slug]) => slug);
+  const sortedSlugs = Object.entries(popularPostsSlugs).map(([slug]) => slug);
 
   const popularPosts = sortedSlugs
     .filter((slug) => !IGNORE_SLUGS.has(slug))
