ba159fc4f5ea5a9442618f149ba3429e064c08e9
hiro <hiro0218@gmail.com>
2024-11-02 02:12:53 +0900

    refactor: 正規表現を関数外に移動

--- src/lib/escapeHTML.ts
+++ src/lib/escapeHTML.ts
@@ -1,13 +1,18 @@
+const escapeMap: { [key: string]: string } = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#x27;',
+  '`': '&#x60;',
+};
+
+const escapeRegExp = /[&<>"'`]/g;
+
 export default function escapeHTML(html: string) {
   if (typeof html !== 'string') {
     return html;
   }
 
-  return html
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#x27;')
-    .replaceAll('`', '&#x60;');
+  return html.replace(escapeRegExp, (match) => escapeMap[match]);
 }
