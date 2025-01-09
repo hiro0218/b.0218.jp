e5ab2b9d636805e8be3f0241eb7291a0ecb7c7c6
hiro <hiro0218@gmail.com>
2025-01-09 10:39:58 +0900

    remove: styled dynamic (move)

--- src/ui/styled/index.ts
+++
@@ -1,6 +0,0 @@
-import { type EmotionCache, Global, css, keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
-
-export { css, Global, keyframes, styled };
-
-export type { EmotionCache };
