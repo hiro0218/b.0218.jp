738e04ae111af70b5be96edbcad3b5ab34df6f1a
hiro <hiro0218@gmail.com>
2025-01-11 01:23:10 +0900

    chore

--- src/components/UI/Adsense/Adsense.tsx
+++ src/components/UI/Adsense/Adsense.tsx
@@ -31,12 +31,16 @@ export function Adsense({ adsWidth = 336, adsHeight = 280 }: Props) {
     }
   }, [pathname, isLoaded]);
 
+  const style = {
+    backgroundColor: process.env.NODE_ENV === 'development' && 'var(--color-gray-3)',
+  };
+
   /**
    * aria-label
    * @link https://support.google.com/adsense/answer/4533986?hl=ja
    */
   return (
-    <Container aria-label="スポンサーリンク" key={pathname}>
+    <Container aria-label="スポンサーリンク" key={pathname} style={style}>
       <Ads adsHeight={adsHeight} adsWidth={adsWidth}>
         <Ins />
       </Ads>
@@ -72,7 +76,6 @@ const Container = styled.aside`
 const Ads = styled.div<Size>`
   min-width: ${({ adsWidth }) => adsWidth}px;
   height: ${({ adsHeight }) => adsHeight}px;
-  background-color: ${() => process.env.NODE_ENV === 'development' && 'var(--color-gray-3)'};
 
   ${isMobile} {
     min-width: 250px;
