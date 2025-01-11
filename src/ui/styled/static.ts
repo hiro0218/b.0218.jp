06bf7458d5c6774a4e73c5ecb90eab42b52b5eaa
hiro <hiro0218@gmail.com>
2025-01-12 01:35:35 +0900

    add: import cx

--- src/ui/styled/static.ts
+++ src/ui/styled/static.ts
@@ -1,4 +1,4 @@
-import { css } from '~/styled-system/css';
+import { css, cx } from '~/styled-system/css';
 import { styled } from '~/styled-system/jsx';
 
-export { css, styled };
+export { css, cx, styled };
