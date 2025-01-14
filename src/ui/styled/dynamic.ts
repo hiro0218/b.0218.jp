393bedacd463ad7a62f19a5301275f9006623a72
hiro <hiro0218@gmail.com>
2025-01-15 00:14:17 +0900

    fix: EmotionCache type export

--- src/ui/styled/dynamic.ts
+++ src/ui/styled/dynamic.ts
@@ -1,6 +1,5 @@
-import type { EmotionCache } from '@emotion/react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export { css, styled };
-export type { EmotionCache };
+export type { EmotionCache } from '@emotion/react';
