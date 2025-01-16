7f299221e3da77d765cd5648ca58bffa1744b58f
hiro <hiro0218@gmail.com>
2025-01-16 18:27:12 +0900

    add: custom media

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'postcss-custom-media-generator': {
+      '--isMobile': `(max-width: ${992 - 1}px)`,
+      '--isDesktop': `(min-width: ${992}px)`,
+    },
+    'postcss-custom-media': {},
     '@pandacss/dev/postcss': {},
   },
-}
+};
