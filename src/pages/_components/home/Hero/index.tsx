e928c48ec433bf655df160e37cca5441b921f0d0
hiro <hiro0218@gmail.com>
2025-01-18 00:15:59 +0900

    refactor: dynamic -> static

--- src/pages/_components/home/Hero/index.tsx
+++ src/pages/_components/home/Hero/index.tsx
@@ -1,7 +1,6 @@
 // import AvatarIcon from '@/assets/hiro0218.svg';
 import { Stack } from '@/components/UI/Layout';
-import { isDesktop, isMobile } from '@/ui/lib/mediaQuery';
-import { styled } from '@/ui/styled/dynamic';
+import { css, styled } from '@/ui/styled/static';
 import Link from 'next/link';
 
 const ICON_SIZE = 100;
@@ -11,7 +10,7 @@ export const Hero = function Hero() {
   const intro = `I'm a web software developer from Japan. I spent 5 years in back-end development, 2 years on iOS and Windows applications, and started my front-end engineering career in 2017.`;
 
   return (
-    <Container align="start" space={2} direction="horizontal">
+    <Stack className={ContainerStyle} align="start" space={2} direction="horizontal">
       <Avatar
         style={{
           // @ts-expect-error CSS Custom Properties
@@ -27,16 +26,16 @@ export const Hero = function Hero() {
         </Heading>
         <p>{intro}</p>
       </Stack>
-    </Container>
+    </Stack>
   );
 };
 
-const Container = styled(Stack)`
+const ContainerStyle = css`
   margin-inline: auto;
   font-size: var(--font-size-md);
   color: var(--color-gray-11);
 
-  ${isDesktop} {
+  @media (--isDesktop) {
     max-width: 85%;
   }
 `;
@@ -46,7 +45,7 @@ const Avatar = styled.div`
   width: var(--size);
   user-select: none;
 
-  ${isMobile} {
+  @media (--isMobile) {
     width: var(--size-shrink);
   }
 
