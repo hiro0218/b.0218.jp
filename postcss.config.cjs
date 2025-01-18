96b48edbc844c3673c2f9c1cf99a5c4d64eb8558
hiro <hiro0218@gmail.com>
2025-01-19 00:59:17 +0900

    add: postcss-mixin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'postcss-mixins': {},
     'postcss-custom-media-generator': {
       '--isMobile': `(max-width: ${992 - 1}px)`,
       '--isDesktop': `(min-width: ${992}px)`,
