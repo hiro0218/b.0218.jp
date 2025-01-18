e81c1f92c06e7121ea4e08ec50d108d00163cf36
hiro <hiro0218@gmail.com>
2025-01-19 01:26:54 +0900

    remove: hash configuration for non-production environments

--- panda.config.ts
+++ panda.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
   lightningcss: true,
   logLevel: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
   minify: true,
-  hash: process.env.NODE_ENV === 'production' ? true : false,
 
   importMap: {
     css: '@/ui/styled/static',
