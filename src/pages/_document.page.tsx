36f3fc3c016e3dfdb1babd79e675b0f4404b3892
hiro <hiro0218@gmail.com>
2025-02-15 02:32:12 +0900

    remove: emotionの処理を削除

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,5 +1,3 @@
-import createEmotionServer from '@emotion/server/create-instance';
-import type { RenderPageResult } from 'next/dist/shared/lib/utils';
 import type { DocumentContext } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { Children } from 'react';
@@ -7,8 +5,6 @@ import { Children } from 'react';
 import { GoogleAdSense } from '@/components/Functional/GoogleAdSense';
 import { MetaLinkFeed, MetaLinkRelMe } from '@/components/Functional/MetaLink';
 import { GOOGLE_ADSENSE } from '@/constant';
-import createEmotionCache from '@/ui/lib/createEmotionCache';
-import { processedCss } from '@/ui/lib/processedCss';
 
 const HTML_PREFIX_BASE = 'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#';
 const HTML_PREFIX_ARTICLE = `${HTML_PREFIX_BASE} article: http://ogp.me/ns/article#`;
@@ -21,39 +17,12 @@ const feeds = [
 
 class MyDocument extends Document<{ ogpPrefix: string }> {
   static async getInitialProps(ctx: DocumentContext) {
-    const originalRenderPage = ctx.renderPage;
-
-    const cache = createEmotionCache();
-    const { extractCriticalToChunks } = createEmotionServer(cache);
-
-    ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
-      originalRenderPage({
-        // biome-ignore lint/suspicious/noExplicitAny:
-        enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
-      });
-
     const initialProps = await Document.getInitialProps(ctx);
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map(({ css, key, ids }) => {
-      const style = processedCss(css);
-      return (
-        style && (
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `@layer emotion { ${style} }`,
-            }}
-            data-emotion={`${key} ${ids.join(' ')}`}
-            key={key}
-          />
-        )
-      );
-    });
-
     const ogpPrefix = ctx.pathname.startsWith('/[slug]') ? HTML_PREFIX_ARTICLE : HTML_PREFIX_BASE;
 
     return {
       ...initialProps,
-      styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
+      styles: [...Children.toArray(initialProps.styles)],
       ogpPrefix,
     };
   }
