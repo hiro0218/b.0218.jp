2199ebc10c0690c51d1c1fb77a100d2e409b5d5b
hiro <hiro0218@gmail.com>
2025-02-15 16:28:08 +0900

    fix(add): return type

--- src/pages/_libs/constant.ts
+++ src/pages/_libs/constant.ts
@@ -2,5 +2,5 @@ export const POST_DISPLAY_LIMIT = 6;
 export const POPULAR_POST_DISPLAY_LIMIT = 6;
 export const UPDATED_POST_DISPLAY_LIMIT = 4;
 
-export const IGNORE_SLUGS = new Set(['20141105125846', '20171223014544']);
-export const IGNORE_TAGS = new Set(['名探偵コナン']);
+export const IGNORE_SLUGS: Set<string> = new Set(['20141105125846', '20171223014544']);
+export const IGNORE_TAGS: Set<string> = new Set(['名探偵コナン']);
