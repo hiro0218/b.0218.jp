049c7cc5d4478a78160e47fc319551a1c12a664d
hiro <hiro0218@gmail.com>
2025-02-17 00:32:23 +0900

    fix(add): return type

--- src/pages/_libs/getTagPosts.ts
+++ src/pages/_libs/getTagPosts.ts
@@ -1,8 +1,12 @@
 import { getPostsJson, getTagsJson } from '@/lib/posts';
+import type { PageProps } from '@/types/source';
+
+type ReturnProps = Omit<PageProps, 'content'>;
+
 const allPosts = getPostsJson();
 const allTags = getTagsJson();
 
-export const getTagPosts = (slug: string) => {
+export const getTagPosts = (slug: string): ReturnProps[] => {
   const tag = allTags[slug];
   const tagPosts = tag.map((postSlug: string) => {
     const { title, date, updated } = allPosts.get(postSlug);
