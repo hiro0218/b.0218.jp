6c89e526a904d6d3f00f3576478e94b44608ca46
hiro <hiro0218@gmail.com>
2025-03-08 01:53:22 +0900

    add: ReturnType

--- src/pages/_libs/getDateAndUpdatedToSimpleFormat.ts
+++ src/pages/_libs/getDateAndUpdatedToSimpleFormat.ts
@@ -1,9 +1,14 @@
 import { convertDateToSimpleFormat } from '@/lib/date';
 
+type ReturnType = {
+  date: string;
+  updated?: string;
+};
+
 /**
  * 日付と更新日を yyyy-mm-dd の形式に変換
  */
-export const getDateAndUpdatedToSimpleFormat = (date: string, updated?: string) => {
+export const getDateAndUpdatedToSimpleFormat = (date: string, updated?: string): ReturnType => {
   return {
     // date を yyyy-mm-dd の形式に変換
     date: convertDateToSimpleFormat(new Date(date)),
