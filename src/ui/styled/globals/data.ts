78cf66519fb78a72121cb82a0245354dcdabb2af
hiro <hiro0218@gmail.com>
2025-03-10 01:17:38 +0900

    add: type

--- src/ui/styled/globals/data.ts
+++ src/ui/styled/globals/data.ts
@@ -1,7 +1,12 @@
 import { SPACE_KEYS, Spaces } from '@/ui/styled/variables/space';
 
-const mtStyles: Record<string, { marginTop: string }> = {};
-const gapStyles: Record<string, { gap: string }> = {};
+type SpacingDataAttr = `data-${'mt' | 'gap'}='${(typeof Spaces)[number]}'`;
+type SpacingCSSProps = Record<'marginTop' | 'gap', `var(${(typeof SPACE_KEYS)[number]})`>;
+type SpacingStyleMap = Record<SpacingDataAttr extends string ? SpacingDataAttr : never, Partial<SpacingCSSProps>>;
+type InitialSpacingStyleMap = Record<SpacingDataAttr, Partial<SpacingCSSProps>>;
+
+const mtStyles: SpacingStyleMap = {} as InitialSpacingStyleMap;
+const gapStyles: SpacingStyleMap = {} as InitialSpacingStyleMap;
 
 Spaces.forEach((value, i) => {
   mtStyles[`[data-mt='${value}']`] = {
