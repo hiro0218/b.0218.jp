271a801eb25f7e4aef77e90c00b9711e5b02aa0a
hiro <hiro0218@gmail.com>
2025-03-16 01:04:09 +0900

    init

---
+++ src/components/Functional/GoogleFontLinks.tsx
@@ -0,0 +1,34 @@
+import uniqueChars from '~/dist/uniqueChars';
+
+/** SafariのURLの最大長は約8,000文字のため超過しないようにする */
+const MAX_URL_LENGTH = 8000;
+const BASE_URL = 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400..900&display=swap';
+
+export const GoogleFontLinks = () => {
+  const links: string[] = [];
+  let currentChunk = '';
+
+  for (const char of uniqueChars) {
+    // 現在の chunk に新しい文字を追加した場合、エンコード後の長さが制限を超えるかをチェック
+    if (encodeURIComponent(currentChunk + char).length > MAX_URL_LENGTH) {
+      links.push(`${BASE_URL}&text=${encodeURIComponent(currentChunk)}`);
+      currentChunk = ''; // 新しい chunk を開始
+    }
+    currentChunk += char;
+  }
+
+  // 最後の chunk を追加
+  if (currentChunk) {
+    links.push(`${BASE_URL}&text=${encodeURIComponent(currentChunk)}`);
+  }
+
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      {links.map((url, index) => (
+        <link key={index} href={url} rel="stylesheet" />
+      ))}
+    </>
+  );
+};
