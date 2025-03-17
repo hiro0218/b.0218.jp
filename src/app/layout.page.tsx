e6b385cfcd6dfff6ade8b488c9302ea443a4ad5b
hiro <hiro0218@gmail.com>
2025-03-17 23:32:56 +0900

    fix: meta

--- src/app/layout.page.tsx
+++ src/app/layout.page.tsx
@@ -3,8 +3,10 @@ import '@/styles/globals.css';
 import { openGraph } from '@/app/metadata';
 import Footer from '@/components/App/Footer';
 import Header from '@/components/App/Header';
+import { GoogleAdSense } from '@/components/Functional/GoogleAdSense';
 import { GoogleFontLinks } from '@/components/Functional/GoogleFontLinks';
-import { AUTHOR_NAME, SITE_DESCRIPTION, SITE_NAME, SITE_URL } from '@/constant';
+import { MetaLinkFeed, MetaLinkRelMe } from '@/components/Functional/MetaLink';
+import { AUTHOR_NAME, GOOGLE_ADSENSE, SITE_DESCRIPTION, SITE_NAME, SITE_URL } from '@/constant';
 import { getOrganizationStructured } from '@/lib/json-ld';
 import { MainContainer } from '@/pages/_components/MainContainer';
 import { Layout } from '@/pages/_layouts/AppLayout';
@@ -13,6 +15,12 @@ import dynamic from 'next/dynamic';
 import type { Metadata, Viewport } from 'next/types';
 
 const PageScroll = dynamic(() => import('@/components/UI/PageScroll').then((module) => module.PageScroll));
+
+const feeds = [
+  { href: '/feed.xml', type: 'application/rss+xml' },
+  { href: '/atom.xml', type: 'application/atom+xml' },
+  { href: '/feed.json', type: 'application/json' },
+];
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -42,7 +50,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <head>
+        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+        <link href="/opensearch.xml" rel="search" type="application/opensearchdescription+xml" />
+        <MetaLinkFeed feeds={feeds} />
         <GoogleFontLinks />
+        <MetaLinkRelMe />
+        <GoogleAdSense publisherId={GOOGLE_ADSENSE.CLIENT} />
         <script
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(getOrganizationStructured()),
