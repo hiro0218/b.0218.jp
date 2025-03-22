5def2d6d205e2b2e94a7dbcdfe8e2b7635d89379
hiro <hiro0218@gmail.com>
2025-03-22 23:07:17 +0900

    replace: content component

--- src/app/(SinglePage)/privacy/page.tsx
+++ src/app/(SinglePage)/privacy/page.tsx
@@ -1,12 +1,8 @@
 import { getMetadata } from '@/app/(SinglePage)/metadata';
-import { parser } from '@/components/Page/Post/Content/inject';
-import { Box } from '@/components/UI/Layout';
-import { Title } from '@/components/UI/Title';
+import Content from '@/components/Page/Single/Content';
 import { SITE_URL } from '@/constant';
-import { getPagesJson } from '@/lib/posts';
 import type { Metadata } from 'next/types';
 
-const pages = getPagesJson();
 const slug = 'privacy';
 const title = 'Privacy';
 const description = 'プライバシーポリシー';
@@ -18,15 +14,5 @@ export const metadata: Metadata = getMetadata({
 });
 
 export default async function Page() {
-  const { content } = pages.find((page) => slug === page.slug);
-  const reactNodeContent = parser(content);
-
-  return (
-    <>
-      <Title heading={title} paragraph={description} />
-      <Box as="article" mt={4} className={'post-content'}>
-        {reactNodeContent}
-      </Box>
-    </>
-  );
+  return <Content title={title} description={description} slug={slug} />;
 }
