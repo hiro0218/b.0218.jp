b5d41719e477217e4676bd7a09ec88c3f608c1dd
hiro <hiro0218@gmail.com>
2025-03-30 03:42:56 +0900

    upgrade: next-sitemapからapp routerへ変更

---
+++ src/app/sitemap.ts
@@ -0,0 +1,44 @@
+import { SITE_URL } from '@/constant';
+import { getPostsListJson } from '@/lib/posts';
+import { getOgpImage, getPermalink } from '@/lib/url';
+import type { MetadataRoute } from 'next';
+
+const posts = getPostsListJson();
+
+export const dynamic = 'force-static';
+
+const pages = [
+  '',
+  'popular',
+  'tags',
+  'archive',
+  // @todo generate from pages.json
+  'about',
+  'privacy',
+] as const;
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const pageList: MetadataRoute.Sitemap = pages.map((page) => {
+    return {
+      url: `${SITE_URL}/${page}`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    };
+  }) as MetadataRoute.Sitemap;
+
+  const postList: MetadataRoute.Sitemap = posts.map((post) => {
+    const ogpImage = `${getOgpImage(post.slug)}`;
+    const permalink = getPermalink(post.slug);
+
+    return {
+      url: permalink,
+      lastModified: post.date,
+      changeFrequency: 'weekly',
+      priority: 0.5,
+      images: [ogpImage],
+    };
+  });
+
+  return [...pageList, ...postList];
+}
