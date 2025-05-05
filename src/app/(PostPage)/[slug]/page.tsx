279e488373438e29d5cd03c4e41891ea3dd26561
hiro <hiro0218@gmail.com>
2025-05-05 17:31:28 +0900

    update: remove Sidebar

--- src/app/(PostPage)/[slug]/page.tsx
+++ src/app/(PostPage)/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import Script from 'next/script';
 import { Container } from '@/components/Functional/Container';
 import {
   Content as PostContent,
@@ -8,14 +11,11 @@ import {
 } from '@/components/Page/Post';
 import { PostSection } from '@/components/Page/Share/PostSection';
 import { TagSection } from '@/components/Page/Share/TagSection';
-import { Sidebar, Stack } from '@/components/UI/Layout';
+import { Stack } from '@/components/UI/Layout';
 import { AUTHOR_NAME, READ_TIME_SUFFIX } from '@/constant';
 import { getBlogPostingStructured, getBreadcrumbStructured, getDescriptionText } from '@/lib/json-ld';
 import { getPostsJson } from '@/lib/posts';
 import { getOgpImage, getPermalink } from '@/lib/url';
-import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import Script from 'next/script';
 import { getData } from './lib/getData';
 
 type Params = Promise<{ slug: string }>;
@@ -55,11 +55,11 @@ export async function generateMetadata({ params }: { params: Params }): Promise<
       images: [{ url: ogpImage }],
     },
     other: {
-      // biome-ignore lint/style/useNamingConvention: <explanation>
+      // biome-ignore lint/style/useNamingConvention: key names should remain unchanged
       updated_time: modifiedTime || publishedTime,
-      // biome-ignore lint/style/useNamingConvention: <explanation>
+      // biome-ignore lint/style/useNamingConvention: key names should remain unchanged
       published_time: publishedTime,
-      // biome-ignore lint/style/useNamingConvention: <explanation>
+      // biome-ignore lint/style/useNamingConvention: key names should remain unchanged
       modified_time: modifiedTime || publishedTime,
       label1: 'Written by',
       data1: AUTHOR_NAME,
@@ -103,53 +103,49 @@ export default async function Page({ params }: { params: Params }) {
         type="application/ld+json"
       />
       {hasTweet && <Script src="https://platform.twitter.com/widgets.js" strategy="lazyOnload" />}
-      <Container size="default" space={false}>
-        <Sidebar space={4}>
-          <Sidebar.Main>
-            <Stack as="article" space={4}>
-              <PostHeader
-                date={date}
-                readingTime={readingTime}
-                render={
-                  <>
-                    <PostShare title={title} url={permalink} />
-                    {!!note && <PostNote note={note} />}
-                  </>
-                }
-                tagsWithCount={tagsWithCount}
-                title={title}
-                updated={updated}
-              />
-              <PostContent content={content} />
-              <Stack direction="horizontal" justify="space-between">
-                <PostShare title={title} url={permalink} />
-                <PostEdit slug={slug} />
-              </Stack>
+      <Container size="small" space={false}>
+        <Stack space={6}>
+          <Stack as="article" space={4}>
+            <PostHeader
+              date={date}
+              readingTime={readingTime}
+              render={
+                <>
+                  <PostShare title={title} url={permalink} />
+                  {!!note && <PostNote note={note} />}
+                </>
+              }
+              tagsWithCount={tagsWithCount}
+              title={title}
+              updated={updated}
+            />
+            <PostContent content={content} />
+            <Stack direction="horizontal" justify="space-between">
+              <PostShare title={title} url={permalink} />
+              <PostEdit slug={slug} />
             </Stack>
-          </Sidebar.Main>
-          <Sidebar.Side>
-            <Stack as="footer" space={5}>
-              <TagSection
-                as="aside"
-                heading="関連タグ"
-                headingLevel="h2"
-                headingWeight="normal"
-                isWideCluster={false}
-                tags={similarTags}
-              />
-              <PostSection as="aside" heading="関連記事" headingLevel="h2" posts={similarPost} updateTarget="date" />
-              <PostSection
-                as="aside"
-                heading="最新記事"
-                headingLevel="h2"
-                href="/archive"
-                posts={recentPosts}
-                updateTarget="date"
-                prefetch={true}
-              />
-            </Stack>
-          </Sidebar.Side>
-        </Sidebar>
+          </Stack>
+          <Stack as="footer" space={5}>
+            <TagSection
+              as="aside"
+              heading="関連タグ"
+              headingLevel="h2"
+              headingWeight="normal"
+              isWideCluster={false}
+              tags={similarTags}
+            />
+            <PostSection as="aside" heading="関連記事" headingLevel="h2" posts={similarPost} updateTarget="date" />
+            <PostSection
+              as="aside"
+              heading="最新記事"
+              headingLevel="h2"
+              href="/archive"
+              posts={recentPosts}
+              updateTarget="date"
+              prefetch={true}
+            />
+          </Stack>
+        </Stack>
       </Container>
     </>
   );
