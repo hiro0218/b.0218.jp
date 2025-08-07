fe0c1cef52adc3abb3931f52a977929a6a8f2c54
hiro <hiro0218@gmail.com>
2025-08-08 00:00:50 +0900

    refactor: move container width variable to font variables

--- src/ui/styled/variables/font/index.ts
+++ src/ui/styled/variables/font/index.ts
@@ -1,7 +1,12 @@
 import { fontSizeHeading } from './heading';
 import { fontSizeVariables } from './sizes';
 
+const globalVars = {
+  '--container-width': '800px',
+};
+
 export const fontVariables = {
+  ...globalVars,
   ...fontSizeVariables,
   ...fontSizeHeading,
 } as const;
