de11c26722e980d19b05412f5ec564e23dfede39
hiro <hiro0218@gmail.com>
2025-09-01 01:51:04 +0900

    refactor: パフォーマンスコストの高い処理を置き換え

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -3,11 +3,24 @@ const isInvalidDate = (date: Date): boolean => {
 };
 
 export const convertDateToSimpleFormat = (date: Date): string => {
-  return date.toLocaleDateString('ja-JP', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  });
+  /**
+   * @workaround パフォーマンスコストが高い場合があるため、toLocaleDateStringの使用を避けている
+   * ```typescript
+   * return date.toLocaleDateString('ja-JP', {
+   *  year: 'numeric',
+   *  month: '2-digit',
+   *  day: '2-digit',
+   * });
+   * ```
+   */
+  const formatDateParts = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}/${month}/${day}`;
+  };
+
+  return formatDateParts(date);
 };
 
 export const isSameDay = (dateA: Date, dateB: Date): boolean => {
@@ -15,5 +28,19 @@ export const isSameDay = (dateA: Date, dateB: Date): boolean => {
     return false;
   }
 
-  return dateA.toLocaleDateString() === dateB.toLocaleDateString();
+  /**
+   * @workaround パフォーマンスコストが高い場合があるため、toLocaleDateStringの使用を避けている
+   * ```typescript
+   * return dateA.toLocaleDateString() === dateB.toLocaleDateString();
+   * ```
+   */
+  const isSameDateParts = (dateA: Date, dateB: Date): boolean => {
+    return (
+      dateA.getFullYear() === dateB.getFullYear() &&
+      dateA.getMonth() === dateB.getMonth() &&
+      dateA.getDate() === dateB.getDate()
+    );
+  };
+
+  return isSameDateParts(dateA, dateB);
 };
