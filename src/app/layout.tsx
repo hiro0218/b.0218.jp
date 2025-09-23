f1f4872c6a782bb40979f8805e44f112ff6a501b
hiro <hiro0218@gmail.com>
2025-09-23 15:12:16 +0900

    fix: viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const PageScroll = dynamic(() => import('@/components/UI/PageScroll').then((modu
 
 export const viewport: Viewport = {
   width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: 'cover',
 };
 
 export const metadata: Metadata = {
