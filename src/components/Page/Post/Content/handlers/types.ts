047a1c0c88a968434b2a17aaa78bf3c1be12e560
hiro <hiro0218@gmail.com>
2026-01-20 22:13:44 +0900

    refactor: add options parameter to HandlerFunction for improved flexibility

--- src/components/Page/Post/Content/handlers/types.ts
+++ src/components/Page/Post/Content/handlers/types.ts
@@ -1,6 +1,9 @@
-import type { Element } from 'html-react-parser';
+import type { Element, HTMLReactParserOptions } from 'html-react-parser';
 
-export type HandlerFunction = (domNode: Element) => React.ReactElement | null | undefined;
+export type HandlerFunction = (
+  domNode: Element,
+  options?: HTMLReactParserOptions,
+) => React.ReactElement | null | undefined;
 
 export type ZoomImageSource = {
   src: string;
