3026a1cc70f82605feee50223b578f8933c94a42
hiro <hiro0218@gmail.com>
2026-01-20 22:09:07 +0900

    refactor: rename isComment to isConditionalComment for clarity

--- build/article/rehypeRemoveComments/index.ts
+++ build/article/rehypeRemoveComments/index.ts
@@ -3,12 +3,12 @@ import { visit } from 'unist-util-visit';
 
 const regex = /^\[if[ \t\f\n\r]+[^\]]+]|<!\[endif]$/;
 
-const isComment = (node: Nodes) => node.type === 'comment' && regex.test(node.value);
+const isConditionalComment = (node: Nodes) => node.type === 'comment' && regex.test(node.value);
 
 export default function rehypeRemoveComments() {
   return function (tree: Root) {
     visit(tree, 'comment', function (node: Comment, index, parent) {
-      if (typeof index === 'number' && parent && !isComment(node)) {
+      if (typeof index === 'number' && parent && !isConditionalComment(node)) {
         parent.children.splice(index, 1);
         return index;
       }
