dbbeaa24a5932aec71545bcf7b64ad3591a15ea7
hiro <hiro0218@gmail.com>
2026-01-26 22:42:54 +0900

    refactor: cashing recent posts data

--- src/app/_lib/index.ts
+++ src/app/_lib/index.ts
@@ -1,19 +1,16 @@
+import { recentPosts, updatesPosts } from '@/app/_lib/cachedRecentPosts';
 import { getFilteredPosts } from '@/app/_lib/getFilteredPosts';
 import { getPopularPost } from '@/app/_lib/getPopularPost';
-import { getRecentAndUpdatedPosts } from '@/app/_lib/getRecentAndUpdatedPosts';
 import { getTagsWithCount } from '@/lib/data/posts';
 
 import { IGNORE_TAGS, POPULAR_POST_DISPLAY_LIMIT } from './constants';
 
 const tagsWithCount = getTagsWithCount();
+const filteredPosts = getFilteredPosts();
+const popularPosts = getPopularPost(filteredPosts, POPULAR_POST_DISPLAY_LIMIT);
+const tags = tagsWithCount.filter(({ slug, count }) => !IGNORE_TAGS.has(slug) && count >= 10).slice(0, 25);
 
 export const getData = () => {
-  const filteredPosts = getFilteredPosts();
-  const { recentPosts, updatesPosts } = getRecentAndUpdatedPosts(filteredPosts);
-  const popularPosts = getPopularPost(filteredPosts, POPULAR_POST_DISPLAY_LIMIT);
-
-  const tags = tagsWithCount.filter(({ slug, count }) => !IGNORE_TAGS.has(slug) && count >= 10).slice(0, 25);
-
   return {
     recentPosts,
     updatesPosts,
