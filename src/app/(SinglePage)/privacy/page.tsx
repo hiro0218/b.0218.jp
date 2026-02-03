1a840d258cf326547da466170551e1694190c49b
hiro <hiro0218@gmail.com>
2026-02-03 18:17:25 +0900

    fix: add type annotation for metadata in about and privacy pages

--- src/app/(SinglePage)/privacy/page.tsx
+++ src/app/(SinglePage)/privacy/page.tsx
@@ -1,8 +1,9 @@
+import type { Metadata } from 'next';
 import { getMetadata, PAGE_CONFIGS } from '@/app/_metadata';
 import Template from '@/components/Page/Single/Template';
 import { SITE_URL } from '@/constants';
 
-export const metadata = getMetadata({
+export const metadata: Metadata = getMetadata({
   title: PAGE_CONFIGS.privacy.title,
   description: PAGE_CONFIGS.privacy.description,
   url: `${SITE_URL}/privacy`,
