8a18b474d39afd19ab34784c1f0324276987fc2c
hiro <hiro0218@gmail.com>
2026-02-09 00:40:17 +0900

    style: wrap Chart component in a new Wrapper styled div for better layout control

--- src/components/Page/Archive/Chart/Chart.tsx
+++ src/components/Page/Archive/Chart/Chart.tsx
@@ -17,57 +17,61 @@ export const Chart = ({ archives, totalPosts }: Props) => {
   }
 
   return (
-    <Container>
-      {years.map((year) => {
-        const percentage = yearPercentages[year];
+    <Wrapper>
+      <Container>
+        {years.map((year) => {
+          const percentage = yearPercentages[year];
 
-        return (
-          <ChartItem key={year} style={{ '--percent': percentage } as React.CSSProperties}>
-            <Anchor className={AnchorStyle} href={`#${year}年`}>
-              {year}
-            </Anchor>
-          </ChartItem>
-        );
-      })}
-    </Container>
+          return (
+            <ChartItem key={year} style={{ '--percent': percentage } as React.CSSProperties}>
+              <Anchor className={AnchorStyle} href={`#${year}年`}>
+                {year}
+              </Anchor>
+            </ChartItem>
+          );
+        })}
+      </Container>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  container-type: inline-size;
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--spacing-½);
-  align-items: start;
-  width: 100%;
-  container-type: inline-size;
 
   @container (min-width: 600px) {
     flex-direction: row;
-    align-items: stretch;
-    width: auto;
     height: var(--spacing-5);
-    overflow-x: scroll;
+    overflow-x: auto;
   }
 `;
 
 const ChartItem = styled.div`
-  /* V8最適化: CSS変数を保持するラッパー要素 */
-  display: contents;
+  flex: none;
+
+  @container (min-width: 600px) {
+    flex: 1;
+    min-width: 5cap;
+  }
 `;
 
 const AnchorStyle = css`
   --fill: var(--colors-gray-100);
   --direction: to right;
 
-  position: relative;
   display: flex;
   align-items: end;
   justify-content: center;
   width: 100%;
-  min-width: 5cap;
   height: 100%;
-  padding: clamp(0px, 0.5vw, var(--spacing-1)) clamp(0px, 0.5vw, var(--spacing-½));
-  font-size: clamp(var(--font-sizes-xs), 1.2vw, var(--font-sizes-sm));
+  padding: var(--spacing-½);
+  font-size: var(--font-sizes-sm);
   font-variant-numeric: tabular-nums;
   line-height: var(--line-heights-sm);
   color: var(--colors-gray-900);
@@ -75,8 +79,6 @@ const AnchorStyle = css`
 
   @container (min-width: 600px) {
     --direction: to top;
-
-    width: auto;
   }
 
   &:hover {
