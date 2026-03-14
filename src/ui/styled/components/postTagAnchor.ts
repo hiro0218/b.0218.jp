13e1d0ce6b85ad8b3ba42ad1fc216b65d2379dc9
hiro <hiro0218@gmail.com>
2026-03-14 23:29:19 +0900

    fix: update button and link styles for touch target sizes

--- src/ui/styled/components/postTagAnchor.ts
+++ src/ui/styled/components/postTagAnchor.ts
@@ -11,8 +11,13 @@ export const postTagAnchor = css`
   color: var(--colors-gray-700);
   text-align: center;
   white-space: nowrap;
+  min-height: var(--sizes-touch-target);
   background-color: var(--colors-gray-a-100);
   border-radius: var(--radii-8);
+
+  @media (--isDesktop) {
+    min-height: auto;
+  }
 
   &:hover {
     background-color: var(--colors-gray-a-200);
