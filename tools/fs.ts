0dff47426d6c1263f31a74060ebb15b80542a6e3
hiro <hiro0218@gmail.com>
2026-05-04 22:39:15 +0900

    refactor: Remove unused copyFile import from fs.ts

--- tools/fs.ts
+++ tools/fs.ts
@@ -1,6 +1,6 @@
-import { copyFile, mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, writeFile } from 'node:fs/promises';
 import { copyDir } from './fs/copyDir';
 import { readJSON } from './fs/readJSON';
 import { writeJSON } from './fs/writeJSON';
 
-export { copyDir, copyFile, mkdir, readJSON, writeFile, writeJSON };
+export { copyDir, mkdir, readJSON, writeFile, writeJSON };
