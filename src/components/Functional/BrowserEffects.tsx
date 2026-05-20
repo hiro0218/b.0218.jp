71e2b3893ed6c14cc30981a258f0d70b11b9acf9
hiro <hiro0218@gmail.com>
2026-05-21 02:13:14 +0900

    refactor: replace useLayoutEffect with useEffect and remove observeScrollbarWidth

--- src/components/Functional/BrowserEffects.tsx
+++ src/components/Functional/BrowserEffects.tsx
@@ -1,22 +1,11 @@
 'use client';
 
-import { useLayoutEffect } from 'react';
-import observeScrollbarWidth from '@/lib/browser/observeScrollbarWidth';
+import { useEffect } from 'react';
 import smoothScroll from '@/lib/browser/smoothScroll';
-import debounce from '@/lib/utils/debounce';
 
 export const BrowserEffects = () => {
-  useLayoutEffect(() => {
-    const cleanupSmoothScroll = smoothScroll();
-    const handleResize = debounce(observeScrollbarWidth, 250);
-
-    observeScrollbarWidth();
-    window.addEventListener('resize', handleResize, { passive: true });
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      cleanupSmoothScroll();
-    };
+  useEffect(() => {
+    return smoothScroll();
   }, []);
 
   return null;
--- src/lib/browser/observeScrollbarWidth.ts
+++
@@ -1,6 +0,0 @@
-export default function observeScrollbarWidth(): void {
-  requestAnimationFrame(() => {
-    const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
-    document.documentElement.style.setProperty('--scrollbar-width', `${scrollBarWidth}px`);
-  });
-}
