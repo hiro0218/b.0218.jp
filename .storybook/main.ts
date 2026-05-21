b90a113f651e678b00ded10e90413f61ca331d46
hiro <hiro0218@gmail.com>
2026-05-22 02:17:21 +0900

    feat: add @storybook/addon-docs to Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/nextjs-vite';
 const config: StorybookConfig = {
   framework: '@storybook/nextjs-vite',
   stories: ['../src/components/UI/**/*.stories.@(ts|tsx)', '../src/stories/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-a11y'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   staticDirs: ['../public'],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
