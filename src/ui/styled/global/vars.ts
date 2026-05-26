97db0a502fb7dc3400f59c0c2cd3184d5076b31f
hiro <hiro0218@gmail.com>
2026-05-27 02:06:08 +0900

    refactor: remove unused container width variable from global styles

--- src/ui/styled/global/vars.ts
+++ src/ui/styled/global/vars.ts
@@ -1,5 +1,4 @@
 const globalVars = {
-  '--container-width': '800px',
   '--resize-dur': '300ms',
   '--resize-ease': 'cubic-bezier(0.22, 1, 0.36, 1)',
   '--transition-fast': 'var(--durations-fast) var(--easings-ease-out-expo)',
